535283cc14c67ff0cc1aaacc89b7f746433f9166
hevia <anthony@hevia.dev>
2024-09-07 19:21:08 -0700

    molten coin maybe done

--- Items/moltenCoin.lua
+++ Items/moltenCoin.lua
@@ -1,6 +1,16 @@
-local sprite = Resources.sprite_load(PATH.."Sprites/Items/detritiveTrematode.png", 1, false, false, 16, 20)
+local sprite = Resources.sprite_load(PATH.."Sprites/Items/moltenCoin.png", 1, false, false, 16, 16)
 
 local item = Item.create("starstorm", "moltenCoin")
 Item.set_sprite(item, sprite)
 Item.set_tier(item, Item.TIER.common)
 Item.set_loot_tags(item, Item.LOOT_TAG.category_damage)
+
+Item.add_callback(item, "onHit", function(actor, victim, damager, stack)
+	if stack > 0 then
+		if Helper.chance(0.06) then
+            --[[ TODO: Play a sound effect here, and grant money
+            ]]
+			Buff.apply(victim, Buff.find("hot"), (4 * stack))
+		end
+	end
+end)
