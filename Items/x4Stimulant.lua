9ba42faaad0b141ea9c38efb8ecaac62d2f91491
hevia <anthony@hevia.dev>
2024-09-07 19:46:23 -0700

    scaffolding for x4 + more sprites

---
+++ Items/x4Stimulant.lua
@@ -0,0 +1,14 @@
+local sprite = Resources.sprite_load(PATH.."Sprites/Items/x4Stimulant.png", 1, false, false, 16, 16)
+
+local item = Item.create("starstorm", "x4Stimulant")
+Item.set_sprite(item, sprite)
+Item.set_tier(item, Item.TIER.common)
+Item.set_loot_tags(item, Item.LOOT_TAG.category_utility)
+
+Item.add_callback(item, "onPickup", function(actor, stack)
+	--[[ TODO: Lower secondary skill cooldown]]
+end)
+
+Item.add_callback(item, "onRemove", function(actor, stack)
+	--[[ TODO: Undo changes to secondary skill cooldown]]
+end)
