20643e8be6fdec7618a0b162b8cf4d48f09adef4
RandomCatDude <kud.stanislav@gmail.com>
2024-10-15 11:46:31 +0500

    add NAMESPACE constant

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ survivor_setup = require("./survivor_setup")
 mods.on_all_mods_loaded(function() for _, m in pairs(mods) do if type(m) == "table" and m.RoRR_Modding_Toolkit then Actor = m.Actor Alarm = m.Alarm Buff = m.Buff Callback = m.Callback Class = m.Class Color = m.Color Equipment = m.Equipment Helper = m.Helper Instance = m.Instance Item = m.Item Net = m.Net Object = m.Object Player = m.Player Resources = m.Resources Skill = m.Skill Survivor = m.Survivor break end end end)
 
 PATH = _ENV["!plugins_mod_folder_path"].."\\"
+NAMESPACE = "ssr"
 
 function __initialize()
 	gm.translate_load_file(gm.variable_global_get("_language_map"), PATH.."Languages\\english.json")
