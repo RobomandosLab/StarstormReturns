b3d9955c165f3e7795e1d0fb8f86841306b61f72
RandomCatDude <kud.stanislav@gmail.com>
2024-10-15 12:06:11 +0500

    cleanup: use new Initialize

--- main.lua
+++ main.lua
@@ -5,15 +5,15 @@ mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
 PATH = _ENV["!plugins_mod_folder_path"].."\\"
 NAMESPACE = "ssr"
 
-function __initialize()
+Initialize(function()
 	gm.sprite_replace(gm.constants.sTitle, path.combine(PATH, "Sprites/Menu/title.png"), 1, false, false, 692/2, 163/2)
 
 	local folders = {
 		--"Survivors",
 		"Items",
 	}
-    for _, folder in ipairs(folders) do
-        local names = path.get_files(PATH..folder)
-        for _, name in ipairs(names) do require(name) end
-    end
-end
+	for _, folder in ipairs(folders) do
+		local names = path.get_files(PATH..folder)
+		for _, name in ipairs(names) do require(name) end
+	end
+end)
