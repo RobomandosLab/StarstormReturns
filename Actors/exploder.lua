e09b7c37c41207746f63e8d050f7eb0229e12284
RandomCatDude <kud.stanislav@gmail.com>
2024-10-22 01:28:03 +0500

    improve exploder, implement new sprites

--- Actors/exploder.lua
+++ Actors/exploder.lua
@@ -1,15 +1,16 @@
 local SPRITE_PATH = path.combine(PATH, "Sprites/Actors/Exploder")
 local SOUND_PATH = path.combine(PATH, "Sounds/Actors/Exploder")
 
-local sprite_mask = Resources.sprite_load(NAMESPACE, "ExploderMask", path.combine(SPRITE_PATH, "mask.png"), 1, 14, 12)
+local sprite_mask = Resources.sprite_load(NAMESPACE, "ExploderMask", path.combine(SPRITE_PATH, "mask.png"), 1, 15, 13)
 local sprite_palette = Resources.sprite_load(NAMESPACE, "ExploderPalette", path.combine(SPRITE_PATH, "palette.png"))
-local sprite_spawn = Resources.sprite_load(NAMESPACE, "ExploderSpawn", path.combine(SPRITE_PATH, "exploderSpawn.png"), 5, 18, 24)
-local sprite_idle = Resources.sprite_load(NAMESPACE, "ExploderIdle", path.combine(SPRITE_PATH, "exploderIdle.png"), 5, 50, 38)
-local sprite_walk = Resources.sprite_load(NAMESPACE, "ExploderWalk", path.combine(SPRITE_PATH, "exploderWalk.png"), 6, 50, 38)
-local sprite_jump = Resources.sprite_load(NAMESPACE, "ExploderJump", path.combine(SPRITE_PATH, "exploderJump.png"), 1, 16, 20)
-local sprite_death = Resources.sprite_load(NAMESPACE, "ExploderDeath", path.combine(SPRITE_PATH, "exploderDeath.png"), 7, 40, 50)
-local sprite_shoot1 = Resources.sprite_load(NAMESPACE, "ExploderShoot1", path.combine(SPRITE_PATH, "exploderShoot1.png"), 20, 40, 50)
-local sprite_impact = Resources.sprite_load(NAMESPACE, "ExploderImpact", path.combine(SPRITE_PATH, "impact.png"), 6, 18, 24)
+local sprite_spawn = Resources.sprite_load(NAMESPACE, "ExploderSpawn", path.combine(SPRITE_PATH, "spawn.png"), 5, 21, 31)
+local sprite_idle = Resources.sprite_load(NAMESPACE, "ExploderIdle", path.combine(SPRITE_PATH, "idle.png"), 6, 15, 14)
+local sprite_walk = Resources.sprite_load(NAMESPACE, "ExploderWalk", path.combine(SPRITE_PATH, "walk.png"), 8, 15, 15)
+local sprite_jump = Resources.sprite_load(NAMESPACE, "ExploderJump", path.combine(SPRITE_PATH, "jump.png"), 1, 18, 14)
+local sprite_death = Resources.sprite_load(NAMESPACE, "ExploderDeath", path.combine(SPRITE_PATH, "death.png"), 7, 32, 55, nil, -16, -16, 16, 10)
+-- attack animation
+local sprite_shoot1a = Resources.sprite_load(NAMESPACE, "ExploderShoot1a", path.combine(SPRITE_PATH, "shoot1a.png"), 15, 32, 55)
+local sprite_shoot1b = Resources.sprite_load(NAMESPACE, "ExploderShoot1b", path.combine(SPRITE_PATH, "shoot1b.png"), 5, 32, 55, nil, -16, -16, 16, 10)
 
 GM.elite_generate_palettes(sprite_palette)
 
@@ -21,6 +22,7 @@ local sound_shoot1b = Resources.sfx_load(NAMESPACE, "ExploderShoot1b", path.comb
 local exploder = Object.new(NAMESPACE, "Exploder", Object.PARENT.enemyClassic)
 local exploder_id = exploder.value
 exploder.obj_sprite = sprite_idle
+exploder.obj_depth = 1 -- depth used by offical enemies, makes players and their vfx always appear above
 
 local exploderPrimary = Skill.new(NAMESPACE, "exploderZ")
 
@@ -49,7 +51,6 @@ exploder:onCreate(function(actor)
 	actor.z_range = 28
 	actor:set_default_skill(Skill.SLOT.primary, exploderPrimary)
 
-
 	actor:init_actor_late()
 end)
 
@@ -63,20 +64,18 @@ stateExploderPrimary:onEnter(function(actor, data)
 end)
 stateExploderPrimary:onStep(function(actor, data)
 	actor:skill_util_fix_hspeed()
-	actor:actor_animation_set(sprite_shoot1, 0.25)
+	actor:actor_animation_set(sprite_shoot1a, 0.25)
 
-	if data.exploded == 0 and actor.image_index >= 15 then
+	if data.exploded == 0 and actor.image_index >= 14 then
 		data.exploded = 1
 
 		GM.sound_play_at(sound_shoot1b, 1.0, (0.9 + math.random() * 0.2) * actor.attack_speed, actor.x, actor.y)
+		GM._mod_game_shakeScreen_global(2)
 
-		-- TODO: destroy the actor at *this* point instead of when the animation ends, replacing it with the explosion
-		actor:fire_explosion(actor.x, actor.y - 16, 120, 56, 2, nil, sprite_impact)
-	end
-end)
-stateExploderPrimary:onGetInterruptPriority(function(actor, data)
-	if data.exploded == 1 then
-		return State.ACTOR_STATE_INTERRUPT_PRIORITY.frozen -- yuck!
+		-- TODO: local explosion attacks exist for making enemy attacks fairer on high ping ... but there's not even _mod_ functions for them..
+		if actor:is_authority() then
+			actor:fire_explosion(actor.x, actor.y - 16, 120, 56, 2)
+		end
 	end
 end)
 
@@ -84,7 +83,16 @@ end)
 Callback.add("postStep", "SSDestroyExploders", function()
 	local exploders = Instance.find_all(exploder)
 	for _, actor in ipairs(exploders) do
-		if actor.actor_state_current_data_table.exploded == 1 and actor.image_index >= 18 then
+		if actor.actor_state_current_data_table.exploded == 1 then
+			-- manually create a corpse. plays the remainder of the explosion animation
+			local body = GM.instance_create(actor.x, actor.y, gm.constants.oBody)
+			body.sprite_index = sprite_shoot1b
+			body.image_xscale = actor.image_xscale
+			body.image_speed = 0.25 * actor.attack_speed
+			body.image_blend = actor.image_blend
+			body.sprite_palette = actor.sprite_palette
+			body.elite_type = actor.elite_type
+
 			actor:destroy()
 		end
 	end
@@ -111,3 +119,12 @@ for _, s in ipairs(stages) do
 	local stage = Stage.find(s)
 	stage:add_monster(monsterCardExploder)
 end
+
+-- for testing purposes hehe ,,
+--[[
+for i, _ in ipairs(Class.STAGE) do
+	local stage = Stage.wrap(i-1)
+	stage:clear_monsters()
+	stage:add_monster(monsterCardExploder)
+end
+]]--
