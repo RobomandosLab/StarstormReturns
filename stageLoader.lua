a11100af52416823f7be1f495848e6633eb4edf1
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2024-10-29 11:48:57 +0100

    Stage Loader

---
+++ stageLoader.lua
@@ -0,0 +1,113 @@
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
+
+PATH = _ENV["!plugins_mod_folder_path"]
+
+NAMESPACE = "ssr"
+
+
+
+Initialize(function()
+    local EnvPlaceholder = Resources.sprite_load(NAMESPACE, "EnvPlaceholder", path.combine(PATH, "EnvironmentPlaceholder.png"))
+
+    --- WHISTLING BASIN ---
+
+    --Stage Resources
+    Resources.sprite_load(NAMESPACE, "tile16basin", path.combine(PATH.."/WhistlingBasin", "tile16basin.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "BackTilesModded2", path.combine(PATH.."/WhistlingBasin", "BackTilesModded2.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "LandCloudWhistlingBasin", path.combine(PATH.."/WhistlingBasin", "LandCloudWhistlingBasin.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "SkyBasin", path.combine(PATH.."/WhistlingBasin", "SkyBasin.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "MoonBasin", path.combine(PATH.."/WhistlingBasin", "MoonBasin.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "MountainsBasinNew", path.combine(PATH.."/WhistlingBasin", "MountainsBasinNew.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "MountainsBasinNew2", path.combine(PATH.."/WhistlingBasin", "MountainsBasinNew2.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "LandCloud4WhistlingBasin", path.combine(PATH.."/WhistlingBasin", "LandCloud4WhistlingBasin.png"), 1, 0, 0)
+    Resources.sprite_load(NAMESPACE, "LandCloud5WhistlingBasin", path.combine(PATH.."/WhistlingBasin", "LandCloud5WhistlingBasin.png"), 1, 0, 0)
+
+    --Menu Resources
+    local EnvironmentWhistlingBasin = Resources.sprite_load(NAMESPACE, "EnvironmentWhistlingBasin", path.combine(PATH.."/WhistlingBasin", "EnvironmentWhistlingBasin.png"))
+    local GroundStripWhistlingBasin = Resources.sprite_load(NAMESPACE, "GroundStripWhistlingBasin", path.combine(PATH.."/WhistlingBasin", "GroundStripWhistlingBasin.png"))
+
+    --Stage
+    local basin_stage = Stage.new(NAMESPACE, "whistlingBasin")
+    basin_stage.music_id = gm.sound_add_w(NAMESPACE, "musicWhistlingBasin", path.combine(PATH.."/WhistlingBasin", "musicWhistlingBasin.ogg"))
+    basin_stage.token_name = "Whistling Basin"
+    basin_stage.token_subname = "Dwindling Oasis" 
+    basin_stage:set_index(2)
+
+    --Rooms
+    basin_stage:add_room(path.combine(PATH.."/WhistlingBasin", "whistlingBasin1.rorlvl"))
+    basin_stage:add_room(path.combine(PATH.."/WhistlingBasin", "whistlingBasin2.rorlvl"))
+    basin_stage:add_room(path.combine(PATH.."/WhistlingBasin", "whistlingBasin3.rorlvl"))
+    basin_stage:add_room(path.combine(PATH.."/WhistlingBasin", "whistlingBasin4.rorlvl"))
+    basin_stage:add_room(path.combine(PATH.."/WhistlingBasin", "whistlingBasin5.rorlvl"))
+
+    --Spawn list
+    basin_stage:add_monster({
+        "lemurian",
+        "wisp",
+        "imp",
+        "crab",
+        "greaterWisp",
+        "ancientWisp",
+        "archaicWisp",
+        "bramble",
+        "scavenger"
+    })
+    basin_stage:add_monster_loop({
+        "impOverlord",
+        "lemrider"
+    })
+
+    basin_stage:add_interactable({
+        "barrel1",
+        "barrelEquipment",
+        "chest1",
+        "chest2",
+        "chest3",
+        "drone3",
+        "drone4",
+        "shrine2",
+        "chestHealing1",
+        "equipmentActivator",
+        "droneRecycler"
+    })
+    basin_stage:add_interactable_loop({
+        "chestHealing2",
+        "chest4",
+        "shrine3S",
+        "barrel2",
+        "chest5"
+    })
+
+    --Environment Log
+    basin_stage:set_log_icon(EnvironmentWhistlingBasin)
+
+    --Main Menu
+    basin_stage:set_title_screen_properties(GroundStripWhistlingBasin)
+--[[
+    --- TORRID OUTLANDS ---
+    Resources.sprite_load(NAMESPACE, "Tile16Outlands", path.combine(PATH.."/TorridOutlands", "Tile16Outlands.png"), 1, 0, 0)
+
+    local outlands_stage = Stage.new(NAMESPACE, "TorridOutlands")
+    outlands_stage.music_id = gm.sound_add_w(NAMESPACE, "musicTorridOutlands", path.combine(PATH.."/TorridOutlands", "musicTorridOutlands.ogg"))
+    outlands_stage.token_name = "Torrid Outlands"
+    outlands_stage.token_subname = "Silent Sunburn" 
+    outlands_stage:set_index(1)
+
+    outlands_stage:add_room(path.combine(PATH.."/TorridOutlands", "torridOutlands_test.rorlvl"))
+
+    outlands_stage:add_monster({
+        "lemurian",
+        "wisp",
+        "imp",
+        "crab",
+        "greaterWisp",
+        "colossus",
+        "archaicWisp",
+        "bramble",
+        "scavenger"
+    })
+
+    outlands_stage:set_log_icon(EnvPlaceholder)
+    ]]
+end)
+
