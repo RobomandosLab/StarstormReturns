be51f73c6b8bb693173d643aa3ae6ad93d86896f
EarthZebra <aus10.hawkeye2@gmail.com>
2025-01-04 23:16:26 -0600

    Technician progress
    Primary kinda jank, Special very close to being done (can be upgraded)

--- Misc/votvlibrary.lua
+++ Misc/votvlibrary.lua
@@ -9,28 +9,42 @@ votv.achievements = {}
 
 --Check if a point is colliding with the stage
 function is_point_colliding_stage(x, y)
-	return gm.place_meeting(x, y, gm.constants.pBlock)
+	local collision = gm.collision_point(x, y, gm.constants.pBlock, false, true)
+	if not collision or (type(collision) == "number" and collision < 0) then
+		return false
+	end
+	return true
 end
 
 function is_colliding_stage(inst, x, y)
-	return inst:is_colliding(gm.constants.pBlock, x, y)
+	return inst:is_colliding(gm.constants.pBlock, x or inst.x, y or inst.y)
 end
 
 --Move a point in a specified direction until it collides with the stage, or has reached the max amount
 ---90 is down, 270 up, 180 left, and 0/360 right
 function move_point_contact_solid(x, y, angle, amount)
 	amount = amount or 1000
+	local totalMoved = 0
 	local xx = math.cos(math.rad(angle))
 	local yy = math.sin(math.rad(angle))
-	while amount > 0 do
-		x = x + xx
-		y = y + yy
+	while totalMoved <= amount do
+		x = x + xx * 32
+		y = y + yy * 32
+		totalMoved = totalMoved + xx * 32 + yy * 32
+		if totalMoved > amount then
+			x = x - xx * (totalMoved - amount)
+			y = y - yy * (totalMoved - amount)
+		end
 		if is_point_colliding_stage(x, y) then
-			x = x - xx
-			y = y - yy
+			for i = 0, 31 do
+				x = x - xx
+				y = y - yy
+				if not is_point_colliding_stage(x, y) then
+					break
+				end
+			end
 			break
 		end
-		amount = amount - 1
 	end
 	return x, y
 end
@@ -38,32 +52,56 @@ end
 --Same as move_point_contact_solid, but stops after *exiting* terrain instead
 function move_point_contact_air(x, y, angle, amount)
 	amount = amount or 1000
+	local totalMoved = 0
 	local xx = math.cos(math.rad(angle))
 	local yy = math.sin(math.rad(angle))
-	while amount > 0 do
-		x = x + xx
-		y = y + yy
+	while totalMoved <= amount do
+		x = x + xx * 32
+		y = y + yy * 32
+		totalMoved = totalMoved + xx * 32 + yy * 32
+		if totalMoved > amount then
+			x = x - xx * (totalMoved - amount)
+			y = y - yy * (totalMoved - amount)
+		end
 		if not is_point_colliding_stage(x, y) then
+			for i = 0, 31 do
+				x = x - xx
+				y = y - yy
+				if is_point_colliding_stage(x, y) then
+					x = x + xx
+					y = y + yy
+					break
+				end
+			end
 			break
 		end
-		amount = amount - 1
 	end
 	return x, y
 end
 
 function move_contact_solid(inst, angle, amount)
 	amount = amount or 1000
+	local totalMoved = 0
 	local xx = math.cos(math.rad(angle))
 	local yy = math.sin(math.rad(angle))
-	while amount > 0 do
-		inst.x = inst.x + xx
-		inst.y = inst.y + yy
+	while totalMoved <= amount do
+		inst.x = inst.x + xx * 32
+		inst.y = inst.y + yy * 32
+		totalMoved = totalMoved + xx * 32 + yy * 32
+		if totalMoved > amount then
+			inst.x = inst.x - xx * (totalMoved - amount)
+			inst.y = inst.y - yy * (totalMoved - amount)
+		end
 		if is_colliding_stage(inst) then
-			inst.x = inst.x - xx
-			inst.y = inst.y - yy
+			for i = 0, 31 do
+				inst.x = inst.x - xx
+				inst.y = inst.y - yy
+				if not is_colliding_stage(inst) then
+					break
+				end
+			end
 			break
 		end
-		amount = amount - 1
 	end
 	return x, y
 end
@@ -71,15 +109,29 @@ end
 
 function move_contact_air(inst, angle, amount)
 	amount = amount or 1000
+	local totalMoved = 0
 	local xx = math.cos(math.rad(angle))
 	local yy = math.sin(math.rad(angle))
-	while amount > 0 do
-		inst.x = inst.x + xx
-		inst.y = inst.y + yy
+	while totalMoved <= amount do
+		inst.x = inst.x + xx * 32
+		inst.y = inst.y + yy * 32
+		totalMoved = totalMoved + xx * 32 + yy * 32
+		if totalMoved > amount then
+			inst.x = inst.x - xx * (totalMoved - amount)
+			inst.y = inst.y - yy * (totalMoved - amount)
+		end
 		if not is_colliding_stage(inst) then
+			for i = 0, 31 do
+				inst.x = inst.x - xx
+				inst.y = inst.y - yy
+				if is_colliding_stage(inst) then
+					inst.x = inst.x + xx
+					inst.y = inst.y + yy
+					break
+				end
+			end
 			break
 		end
-		amount = amount - 1
 	end
 	return x, y
 end
@@ -110,4 +162,21 @@ end
 --Adds X to the achievement's progress (Achievement completes when progress reaches the requirement)
 function progress_achievement(achievement, value)
 	gm.achievement_add_progress(achievement.value, value)
+end
+
+function tableToString(t)
+    local function serialize(tbl, indent)
+        local str = ""
+        indent = indent or ""
+        for k, v in pairs(tbl) do
+            str = str .. indent .. k .. ": "
+            if type(v) == "table" then
+                str = str .. "\n" .. serialize(v, indent .. "  ")
+            else
+                str = str .. tostring(v) .. "\n"
+            end
+        end
+        return str
+    end
+    return serialize(t)
 end
--- Survivors/technician.lua
+++ Survivors/technician.lua
@@ -10,9 +10,9 @@ local sprite_portrait		= Resources.sprite_load(NAMESPACE, "TechnicianPortrait", 
 local sprite_portrait_small	= Resources.sprite_load(NAMESPACE, "TechnicianPortraitSmall", path.combine(SPRITE_PATH, "portraitSmall.png"))
 local sprite_skills			= Resources.sprite_load(NAMESPACE, "TechnicianSkills", path.combine(SPRITE_PATH, "skills.png"), 9)
 
-local sprite_idle			= Resources.sprite_load(NAMESPACE, "TechnicianIdle", path.combine(SPRITE_PATH, "idle.png"), 1, 12, 17)
+local sprite_idle			= Resources.sprite_load(NAMESPACE, "TechnicianIdle", path.combine(SPRITE_PATH, "idle.png"), 1, 8, 12)
 local sprite_idle_half		= Resources.sprite_load(NAMESPACE, "TechnicianIdleHalf", path.combine(SPRITE_PATH, "idleHalf.png"), 1, 12, 17)
-local sprite_walk			= Resources.sprite_load(NAMESPACE, "TechnicianWalk", path.combine(SPRITE_PATH, "walk.png"), 8, 14, 18)
+local sprite_walk			= Resources.sprite_load(NAMESPACE, "TechnicianWalk", path.combine(SPRITE_PATH, "walk.png"), 8, 10, 14)
 local sprite_walk_half		= Resources.sprite_load(NAMESPACE, "TechnicianWalkHalf", path.combine(SPRITE_PATH, "walkHalf.png"), 8, 14, 18)
 local sprite_walk_back		= Resources.sprite_load(NAMESPACE, "TechnicianWalkBack", path.combine(SPRITE_PATH, "walkBack.png"), 8, 14, 18)
 local sprite_jump			= Resources.sprite_load(NAMESPACE, "TechnicianJump", path.combine(SPRITE_PATH, "jump.png"), 1, 12, 15)
@@ -25,21 +25,28 @@ local sprite_climb			= Resources.sprite_load(NAMESPACE, "TechnicianClimb", path.
 local sprite_death			= Resources.sprite_load(NAMESPACE, "TechnicianDeath", path.combine(SPRITE_PATH, "death.png"), 5, 14, 8)
 local sprite_decoy			= Resources.sprite_load(NAMESPACE, "TechnicianDecoy", path.combine(SPRITE_PATH, "decoy.png"), 1, 17, 16)
 
-local sprite_shoot1			= Resources.sprite_load(NAMESPACE, "TechnicianShoot1", path.combine(SPRITE_PATH, "shoot1.png"), 5, 10, 17)
+local sprite_shoot1_1		= Resources.sprite_load(NAMESPACE, "TechnicianShoot1_1", path.combine(SPRITE_PATH, "shoot1_1.png"), 5, 12, 24)
+local sprite_shoot1_2		= Resources.sprite_load(NAMESPACE, "TechnicianShoot1_2", path.combine(SPRITE_PATH, "shoot1_2.png"), 5, 12, 24)
 local sprite_shoot1_half	= Resources.sprite_load(NAMESPACE, "TechnicianShoot1Half", path.combine(SPRITE_PATH, "shoot1Half.png"), 5, 10, 17)
 local sprite_shoot2a		= Resources.sprite_load(NAMESPACE, "TechnicianShoot2a", path.combine(SPRITE_PATH, "shoot2a.png"), 6, 12, 25)
 local sprite_shoot2b		= Resources.sprite_load(NAMESPACE, "TechnicianShoot2b", path.combine(SPRITE_PATH, "shoot2b.png"), 6, 12, 25)
 local sprite_shoot3			= Resources.sprite_load(NAMESPACE, "TechnicianShoot3", path.combine(SPRITE_PATH, "shoot3.png"), 8, 44, 40)
-local sprite_shoot4			= Resources.sprite_load(NAMESPACE, "TechnicianShoot4", path.combine(SPRITE_PATH, "shoot4.png"), 22, 39, 63)
+local sprite_shoot4			= Resources.sprite_load(NAMESPACE, "TechnicianShoot4", path.combine(SPRITE_PATH, "shoot4.png"), 6, 8, 14)
 local sprite_shoot5			= Resources.sprite_load(NAMESPACE, "TechnicianShoot4S", path.combine(SPRITE_PATH, "shoot5.png"), 22, 39, 63)
 
 local sprite_sparks2		= Resources.sprite_load(NAMESPACE, "TechnicianSparks", path.combine(SPRITE_PATH, "sparks2.png"), 4, 24, 14)
 local sprite_tracer2		= Resources.sprite_load(NAMESPACE, "TechnicianIonTracer", path.combine(SPRITE_PATH, "tracer2.png"), 5, 0, 2)
 
-local sprite_turretaI		= Resources.sprite_load(NAMESPACE, "TechnicianTurretaIdle", path.combine(SPRITE_PATH, "turreta.png"), 2, 0, 2)
-local sprite_turretashoot	= Resources.sprite_load(NAMESPACE, "TechnicianTurretaShoot", path.combine(SPRITE_PATH, "turretashoot.png"), 4, 0, 2)
-local sprite_turretexplosion= Resources.sprite_load(NAMESPACE, "TechnicianTurretExplosion", path.combine(SPRITE_PATH, "turretexplosion.png"), 4, 0, 2)
-local sprite_turretsparks	= Resources.sprite_load(NAMESPACE, "TechnicianTurretSparks", path.combine(SPRITE_PATH, "turretsparks.png"), 3, 0, 2)
+local sprite_turretaI		= Resources.sprite_load(NAMESPACE, "TechnicianTurretaIdle", path.combine(SPRITE_PATH, "turreta.png"), 2, 14, 14)
+local sprite_turretashoot	= Resources.sprite_load(NAMESPACE, "TechnicianTurretaShoot", path.combine(SPRITE_PATH, "turretashoot.png"), 4, 14, 14)
+local sprite_turretexplosion= Resources.sprite_load(NAMESPACE, "TechnicianTurretExplosion", path.combine(SPRITE_PATH, "turretexplosion.png"), 4, 20, 20)
+local sprite_turretsparks	= Resources.sprite_load(NAMESPACE, "TechnicianTurretSparks", path.combine(SPRITE_PATH, "turretsparks.png"), 3, 26, 16)
+
+local sprite_turretbI		= Resources.sprite_load(NAMESPACE, "TechnicianTurretbIdle", path.combine(SPRITE_PATH, "turretb.png"), 2, 16, 18)
+local sprite_turretbshoot	= Resources.sprite_load(NAMESPACE, "TechnicianTurretbshoot", path.combine(SPRITE_PATH, "turretbshoot.png"), 4, 18, 18)
+
+local sprite_turretcI		= Resources.sprite_load(NAMESPACE, "TechnicianTurretcIdle", path.combine(SPRITE_PATH, "turretc.png"), 2, 16, 18)
+local sprite_turretcshoot	= Resources.sprite_load(NAMESPACE, "TechnicianTurretcshoot", path.combine(SPRITE_PATH, "turretcshoot.png"), 4, 18, 18)
 
 local sound_shoot1			= Resources.sfx_load(NAMESPACE, "TechnicianShoot1", path.combine(SOUND_PATH, "skill1.ogg"))
 local sound_shoot2			= Resources.sfx_load(NAMESPACE, "TechnicianShoot2", path.combine(SOUND_PATH, "skill2.ogg"))
@@ -110,7 +117,86 @@ local technicianSecondary = technician:get_secondary()
 local technicianUtility = technician:get_utility()
 local technicianSpecial = technician:get_special()
 
--- Service Pistol
+-- machines
+local obj_turret = Object.new(NAMESPACE, "turret")
+obj_turret:set_sprite(sprite_turretaI)
+obj_turret:set_depth(1)
+obj_turret:onCreate(function(inst)
+	local data = inst:get_data()
+	data.idle = sprite_turretaI
+	data.shoot = sprite_turretashoot
+	data.init = nil
+	data.hits_taken = 0
+	inst.image_speed = 0.2
+	data.cooldown = 0
+	data.basecooldown = 50
+	data.damage = technicianSpecial.damage
+	data.upgradeState = 0
+end)
+obj_turret:onStep(function(inst)
+	local data = inst:get_data()
+	if data.parent then
+		-- print("TRY AGAIN!")
+		data.scepter = data.parent:item_stack_count(item_scepter)
+		if not data.init then
+			local xx,_ = move_point_contact_solid(inst.x, inst.y, 90 - 90 * inst.image_xscale)
+			data.range = math.min(math.abs(xx - inst.x), 1000)
+			print(data.range)
+			data.init = 1
+			print("xx "..(xx).." inst.x "..(inst.x))
+		end
+		if data.hits_taken >= 3 and data.upgradeState <= 0 then
+			data.idle = sprite_turretbI
+			data.shoot = sprite_turretbshoot
+			data.basecooldown = 12
+			data.damage = 0.7
+			data.upgradeState = 1
+			data.cooldown = math.min(data.cooldown, data.basecooldown)
+		end
+		if data.hits_taken >= 6 and data.upgradeState <= 1 and data.scepter > 0 then
+			data.idle = sprite_turretcI
+			data.shoot = sprite_turretcshoot
+			data.upgradeState = 2
+			data.basecooldown = 8
+			data.cooldown = math.min(data.cooldown, data.basecooldown)
+		end
+		local victims = List.new()
+		inst:collision_line_list(inst.x, inst.y, inst.x + data.range * inst.image_xscale, inst.y, gm.constants.pActor, false, true, victims, false)
+		local wantattack = false
+		for _, victim in ipairs(victims) do
+			if victim.team ~= data.team then
+				wantattack = true
+				break
+			end
+		end
+		data.cooldown = data.cooldown - 1
+		if data.playanim then
+			inst.sprite_index = data.shoot
+			if inst.image_index >= gm.sprite_get_number(data.shoot) - 1 then
+				data.playanim = nil
+			end
+		else
+			inst.sprite_index = data.idle
+		end
+		if wantattack then
+			if data.cooldown <= 0 then
+				data.playanim = 1
+				inst.image_index = 0
+				data.cooldown = data.basecooldown
+				for i = 0, data.parent:buff_stack_count(buff_mirror) do
+					local attack = data.parent:fire_bullet(inst.x, inst.y, 1000, 90 - 90 * inst.image_xscale, data.damage, nil, sprite_turretsparks, Attack_Info.TRACER.commando1)
+					attack.climb = i * 8
+				end
+			end
+		end
+	else
+		inst:destroy()
+	end
+end)
+
+local machines = {obj_turret}
+
+-- Wrench Whack
 technicianPrimary.sprite = sprite_skills
 technicianPrimary.subimage = 0
 
@@ -133,15 +219,18 @@ stateTechnicianPrimary:clear_callbacks()
 stateTechnicianPrimary:onEnter(function(actor, data)
 	actor.image_index2 = 0
 	data.fired = 0 -- gamemaker bools are a pain to deal with in lua, so just use numbers instead
-
+	data.currentAnim = ((data.currentAnim or 1) + 1) % 2
 	actor:skill_util_strafe_init()
 	actor:skill_util_strafe_turn_init()
 end)
 
+local primaryOffset = get_tiles(0.6)
+local primaryWidth = get_tiles(1.2)
+local primaryHeight = get_tiles(0.7)
 stateTechnicianPrimary:onStep(function(actor, data)
-	actor.sprite_index2 = sprite_shoot1_half
+	actor.sprite_index2 = (data.currentAnim == 1 and sprite_shoot1_2 or sprite_shoot1_1)
 
-	actor:skill_util_strafe_update(0.33 * actor.attack_speed, 0.5)
+	actor:skill_util_strafe_update(0.2 * actor.attack_speed, 0.5)
 	actor:skill_util_step_strafe_sprites()
 	actor:skill_util_strafe_turn_update()
 
@@ -157,7 +246,7 @@ stateTechnicianPrimary:onStep(function(actor, data)
 		actor.ydisp = walk_offset -- ydisp controls upper body offset
 	end
 
-	if data.fired == 0 then
+	if data.fired == 0 and actor.image_index >= 1 then
 		data.fired = 1
 
 		actor:sound_play(sound_shoot1, 1, 0.9 + math.random() * 0.2)
@@ -168,8 +257,28 @@ stateTechnicianPrimary:onStep(function(actor, data)
 
 			if not GM.skill_util_update_heaven_cracker(actor, damage, actor.image_xscale) then
 				for i=0, actor:buff_stack_count(buff_mirror) do
-					local attack = actor:fire_explosion(actor.x + get_tiles(0.6) * actor.image_xscale, actor.y, get_tiles(1.2), get_tiles(0.7), actor:skill_get_damage(technicianPrimary)).attack_info
-					attack.climb = i * 8
+					local attack = actor:fire_explosion(actor.x + primaryOffset * actor.image_xscale, actor.y, primaryWidth, primaryHeight, actor:skill_get_damage(technicianPrimary))
+					local attackinfo = attack.attack_info
+					attackinfo.climb = i * 8
+					--[[ local machinesHit = List.new()
+					for _, machineType in ipairs(machines) do
+						actor:collision_rectangle_list(actor.x + (primaryOffset - primaryWidth / 2) * actor.image_xscale, actor.y - primaryHeight / 2, actor.x + (primaryOffset + primaryWidth / 2) * actor.image_xscale, actor.y + primaryHeight / 2, machineType, false, true, machinesHit, false)
+					-- ^ finding all machines in machines table that collides with the explosion to upgrade 
+					end
+					print(" Machines ")
+					print(tableToString(machines))
+					print(" Machines Hit ")
+					print(tableToString(machinesHit)) ]]
+					for _, machineInst in ipairs(Instance.find_all(machines)) do
+						local xDelta = machineInst.x - actor.x
+						if xDelta <= (primaryOffset + primaryWidth / 2) and math.abs(machineInst.y - actor.y) <= (primaryHeight / 2) and( (actor.image_xscale < 0 and xDelta < 0) or (actor.image_xscale >= 0 and xDelta >= 0)) then
+							local machineData = machineInst:get_data()
+							if machineData.team == actor.team then
+								machineData.hits_taken = machineData.hits_taken + 1
+								print((machineData.hits_taken).. " Hits Taken ")
+							end
+						end
+					end
 				end
 			end
 		end
@@ -385,40 +494,7 @@ end)
 
 -- Backup Firewall
 
-local obj_turret = Object.new(NAMESPACE, "turret")
-obj_turret:set_sprite(sprite_turretaI)
-obj_turret:set_depth(1)
-obj_turret:onCreate(function(inst)
-	local data = inst:get_data()
-	data.init = nil
-	data.hits_taken = 0
-	inst.image_speed = 0.2
-	data.cooldown = 0
-end)
-obj_turret:onStep(function(inst)
-	local data = inst:get_data()
-	if data.parent then
-		if not data.init then
-			local xx,_ = move_point_contact_solid(inst.x, inst.y, 90 + 90 * inst.image_xscale)
-			data.range = math.min(math.abs(xx - inst.x), 1000)
-			data.init = 1
-		end
-		local victims = List.new()
-		inst:collision_line_list(inst.x, inst.y, inst.x + data.range * inst.image_xscale, inst.y, gm.constants.pActor, false, true, victims, false)
-		if #victims > 0 then
-			data.cooldown = data.cooldown - 1
-			if data.cooldown <= 0 then
-				data.cooldown = 50
-				for i = 0, data.parent:buff_stack_count(buff_mirror) do
-					local attack = data.parent:fire_bullet(inst.x, inst.y, 1000, 90 + 90 * inst.image_xscale, technicianSpecial.damage, nil, sprite_turretsparks, Attack_Info.TRACER.commando1)
-					attack.climb = i * 8
-				end
-			end
-		end
-	else
-		inst:destroy()
-	end
-end)
+
 
 technicianSpecial.sprite = sprite_skills
 technicianSpecial.subimage = 7
@@ -463,18 +539,22 @@ stateTechnicianSpecial:onStep(function(actor, data)
 	if data.scepter > 0 then
 		animation = sprite_shoot5
 	end
-	if not data.created then
+	if not data.created and actor.image_index >= 4 then
 		local turrets, _ = Instance.find_all(obj_turret)
 		for _, turret in ipairs(turrets) do
 			if turret:get_data().parent.id == actor.id then
 				turret:destroy()
 			end
 		end
-		local turret_inst = obj_turret:create(actor.x, actor.y)
+		local turret_inst = obj_turret:create(actor.x + 5 * actor.image_xscale, actor.y - 8)
 		local turret_data = turret_inst:get_data()
 		turret_data.parent = actor
+		turret_data.team = actor.team
 		turret_inst.image_xscale = actor.image_xscale
 		move_contact_solid(turret_inst, 90)
+		if data.scepter > 0 then
+			turret_data.hits_taken = 3
+		end
 		data.created = 1
 	end
 	actor:actor_animation_set(animation, 0.25, true)
