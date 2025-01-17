51222b45b26fc22192d96ac7ea0e091ea7c304e7
kris <kud.stanislav@gmail.com>
2025-01-18 01:50:44 +0500

    poison elite

--- Elites/poison.lua
+++ Elites/poison.lua
@@ -1,67 +1,77 @@
+local SPRITE_PATH = path.combine(PATH, "Sprites/Elites/Poison")
+local SOUND_PATH = path.combine(PATH, "Sounds/Elites/Poison")
 
+local sprite_icon = Resources.sprite_load(NAMESPACE, "EliteIconPoison", path.combine(SPRITE_PATH, "icon.png"), 1, 14, 10)
+local sprite_palette = Resources.sprite_load(NAMESPACE, "ElitePalettePoison", path.combine(SPRITE_PATH, "palette.png"))
 
--- local palette = gm.sprite_add(PATH.."Sprites/Elites/Poison/poisonPalette.png", 1, false, false, 0, 0) --determines the colour of the enemy and stuff, look at vanilla elite palettes for reference (not player palettes, those r different it seems)
--- local elite_id = gm.elite_type_create("starstorm", "poison")
--- local elite_class = gm.variable_global_get("class_elite")
+local sound_poison_cloud = Resources.sfx_load(NAMESPACE, "PoisonCloud", path.combine(SOUND_PATH, "poisonCloud.ogg"))
+local sound_poison_inflict = Resources.sfx_load(NAMESPACE, "PoisonInflict", path.combine(SOUND_PATH, "poisonInflict.ogg"))
 
--- --using frenzied elite as a base for a lot of things, i havent looked into it too hard but the class dump is a good reference otherwise
--- local brainworms = elite_class[3] 
--- local test_me = elite_class[elite_id+1]
--- for i = 0, 10, 1 do
---     if i > 3 and i ~= 7 then
---         gm.array_set(test_me, i-1, brainworms[i])
---     end
--- end
--- gm.array_set(test_me, 3, palette)
--- gm.array_set(test_me, 4, palette)
--- local eliteNate = gm.elite_type_find("poison")
+local elitePoison = Elite.new(NAMESPACE, "poison")
+elitePoison.healthbar_icon = sprite_icon
+elitePoison.palette = sprite_palette
+elitePoison.blend_col = Color.PURPLE
 
--- --applying elite types, i sort of took some of this code from blighted worm and modified it
--- local monster_cards = gm.variable_global_get("class_monster_card") 
--- for i=1, #monster_cards do
---     local card = monster_cards[i]
---     local elite_types = gm.array_get(card, 7)
---     gm.ds_list_add(elite_types, eliteNate)
--- end
+GM.elite_generate_palettes()
 
--- gm._mod_elite_generate_palette_all()
--- gm.elite_generate_palettes(elite_id) --havent tested too much, you probably only need one of these instead of both
+local itemEliteOrbPoison = Item.new(NAMESPACE, "eliteOrbPoison", true) -- true for no logbook
+itemEliteOrbPoison.is_hidden = true
 
--- --custom elite item, ignore this mostly since i havent touched this much. this would probably also be how you handle a lot of the custom behaviour of the elites
--- --tier is 7, sprite id is 1628, object id is -1, 0 loot tags
--- local item = Item.create("starstorm", "poisonEliteItem")
+elitePoison:clear_callbacks()
+elitePoison:onApply(function(actor)
+	actor:item_give(itemEliteOrbPoison)
+end)
 
--- Item.add_callback(item, "onPickup", function (actor)
---     log.info("Starstorm poison elite item onPickup triggered!!")
--- end)
+itemEliteOrbPoison:clear_callbacks()
+itemEliteOrbPoison:onHitProc(function(actor, victim, stack, hit_info)
+	GM.sound_play_networked(sound_poison_inflict, 1, 0.9 + math.random() * 0.2, victim.x, victim.y)
 
--- Item.add_callback(item, "onStep", function (actor, stack)
---     if stack > 0 then
---         if not actor.ssr_poison_cooldown then
---             log.info("Poison triggered!!")
---             actor.ssr_poison_cooldown = 10
-            
---             -- TODO: Clean up this code
---             local t = gm.instance_create(actor.x, actor.y, gm.constants.oMushDust)
---             t.parent = actor
---             t.team = actor.team
---             t.damage = 5 --damage / 3 * (critical + 1)
---             --t.critical = critical
--- 		else
--- 			if actor.ssr_poison_cooldown > 0 then
--- 				actor.ssr_poison_cooldown = actor.ssr_poison_cooldown - 1
--- 			else
--- 				actor.ssr_poison_cooldown = nil
--- 			end
--- 		end
-        
---     end
--- end)
+	local dot = GM.instance_create(victim.x, victim.y, gm.constants.oDot)
+	dot.parent = actor
+	dot.team = actor.team
+	dot.target = victim
 
--- local id = Item.find("starstorm", "poisonEliteItem")
--- Callback.add("onEliteInit", "starstorm.elite_init", function (actor)
---     --need to maket this apply to blighted too
---     if gm.actor_get_elite_type(actor) == elite_id then
---         gm.item_give_internal(actor, id, 1, Item.TYPE.real)
---     end
--- end)
+	dot.damage = math.ceil(victim.maxhp * 0.03)
+	dot.ticks = 4
+	dot.rate = 60
+	dot.textColor = Color.PURPLE
+
+	dot:alarm_set(0, dot.rate) -- oDot usually does its first tick instantly -- make it delayed
+end)
+itemEliteOrbPoison:onPostStep(function(actor, stack)
+	if gm._mod_net_isClient() then return end
+
+	local data = actor:get_data()
+	if not data.poison_timer then
+		data.poison_timer = 30
+	end
+
+	data.poison_timer = data.poison_timer - 1
+	if data.poison_timer < 0 then
+		data.poison_timer = 30 + math.random(30)
+
+		if math.random() < 0.05 then
+			GM.sound_play_networked(sound_poison_cloud, 1, 0.9 + math.random() * 0.2, actor.x, actor.y)
+
+			local cloud = GM.instance_create(actor.x, actor.y, gm.constants.oMushDust)
+			cloud.parent = actor
+			cloud.team = actor.team
+			cloud.damage = actor.damage * 0.3
+			cloud:alarm_set(0, 60 * 5) -- set lifetime
+		end
+	end
+end)
+
+local blacklist = {
+	["magmaWorm"] = true,
+}
+
+local all_monster_cards = Monster_Card.find_all()
+for i, card in ipairs(all_monster_cards) do
+	if not blacklist[card.identifier] then
+		local elite_list = List.wrap(card.elite_list)
+		if not elite_list:contains(elitePoison) then
+			elite_list:add(elitePoison)
+		end
+	end
+end
