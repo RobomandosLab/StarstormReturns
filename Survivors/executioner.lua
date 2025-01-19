5560a4c2316f98d8374671dd4b8da3aee8afe5ef
kris <kud.stanislav@gmail.com>
2025-01-19 22:51:28 +0500

    a bit of light code cleanup

--- Survivors/executioner.lua
+++ Survivors/executioner.lua
@@ -1,7 +1,7 @@
 local SPRITE_PATH = path.combine(PATH, "Sprites/Survivors/Executioner")
 local SOUND_PATH = path.combine(PATH, "Sounds/Survivors/Executioner")
 
--- assets.
+-- sprites.
 local sprite_loadout		= Resources.sprite_load(NAMESPACE, "ExecutionerSelect", path.combine(SPRITE_PATH, "select.png"), 22, 28, 0)
 local sprite_portrait		= Resources.sprite_load(NAMESPACE, "ExecutionerPortrait", path.combine(SPRITE_PATH, "portrait.png"), 3)
 local sprite_portrait_small	= Resources.sprite_load(NAMESPACE, "ExecutionerPortraitSmall", path.combine(SPRITE_PATH, "portraitSmall.png"))
@@ -43,19 +43,25 @@ local sprite_shoot5			= Resources.sprite_load(NAMESPACE, "ExecutionerShoot5", pa
 local sprite_drone_idle		= Resources.sprite_load(NAMESPACE, "DronePlayerExecutionerIdle", path.combine(SPRITE_PATH, "droneIdle.png"), 5, 11, 14)
 local sprite_drone_shoot	= Resources.sprite_load(NAMESPACE, "DronePlayerExecutionerShoot", path.combine(SPRITE_PATH, "droneShoot.png"), 5, 33, 13)
 
-local sprite_sparks2		= Resources.sprite_load(NAMESPACE, "ExecutionerSparks", path.combine(SPRITE_PATH, "sparks2.png"), 4, 24, 14)
-local sprite_tracer2		= Resources.sprite_load(NAMESPACE, "ExecutionerIonTracer", path.combine(SPRITE_PATH, "tracer2.png"), 5, 0, 2)
+local sprite_ion_sparks		= Resources.sprite_load(NAMESPACE, "ExecutionerIonSparks", path.combine(SPRITE_PATH, "ionSparks.png"), 4, 24, 14)
+local sprite_ion_tracer		= Resources.sprite_load(NAMESPACE, "ExecutionerIonTracer", path.combine(SPRITE_PATH, "ionTracer.png"), 5, 0, 2)
 local sprite_ion_particle	= Resources.sprite_load(NAMESPACE, "ExecutionerIonParticle", path.combine(SPRITE_PATH, "ionParticle.png"), 5, 8, 8)
 local sprite_ion_particleS	= Resources.sprite_load(NAMESPACE, "ExecutionerIonParticleS", path.combine(SPRITE_PATH, "ionParticleS.png"), 5, 8, 8)
 
 local sprite_log			= Resources.sprite_load(NAMESPACE, "ExecutionerLog", path.combine(SPRITE_PATH, "log.png"))
 
+-- sounds.
 local sound_shoot1			= Resources.sfx_load(NAMESPACE, "ExecutionerShoot1", path.combine(SOUND_PATH, "skill1.ogg"))
 local sound_shoot2			= Resources.sfx_load(NAMESPACE, "ExecutionerShoot2", path.combine(SOUND_PATH, "skill2.ogg"))
 local sound_shoot3			= Resources.sfx_load(NAMESPACE, "ExecutionerShoot3", path.combine(SOUND_PATH, "skill3.ogg"))
 local sound_shoot4a			= Resources.sfx_load(NAMESPACE, "ExecutionerShoot4a", path.combine(SOUND_PATH, "skill4a.ogg"))
 local sound_shoot4b			= Resources.sfx_load(NAMESPACE, "ExecutionerShoot4b", path.combine(SOUND_PATH, "skill4b.ogg"))
 
+-- particles.
+-- used for ion burst tracer
+local particleWispGTracer = Particle.find("ror", "WispGTracer")
+
+-- used for crowd dispersion and execution
 local ionParticle = Particle.new(NAMESPACE, "ion")
 ionParticle:set_sprite(sprite_ion_particle, true, true, false)
 ionParticle:set_life(15, 60)
@@ -64,6 +70,7 @@ ionParticle:set_speed(0.2, 0.5, -0.02, 0)
 ionParticle:set_size(0.6, 1, 0, 0.01)
 ionParticle:set_direction(0, 360, 0, 0)
 
+-- used for scepter-boosted execution
 local ionParticleS = Particle.new(NAMESPACE, "ionS")
 ionParticleS:set_sprite(sprite_ion_particleS, true, true, false)
 ionParticleS:set_life(15, 60)
@@ -223,8 +230,8 @@ executionerSecondary.start_with_stock = false
 executionerSecondary.use_delay = 30
 executionerSecondary.required_interrupt_priority = State.ACTOR_STATE_INTERRUPT_PRIORITY.skill
 
-local tracer_particle = Particle.find("ror", "WispGTracer")
-local tracer_color = Color.from_rgb(110, 129, 195)
+local ION_TRACER_COLOR = Color.from_rgb(110, 129, 195)
+
 local ion_tracer, ion_tracer_info = CustomTracer.new(function(x1, y1, x2, y2)
 	if x1 < x2 then x1 = x1 + 16 else x1 = x1 - 16 end
 
@@ -236,11 +243,11 @@ local ion_tracer, ion_tracer_info = CustomTracer.new(function(x1, y1, x2, y2)
 
 	tracer.xend = x2
 	tracer.yend = y2
-	tracer.sprite_index = sprite_tracer2
+	tracer.sprite_index = sprite_ion_tracer
 	tracer.image_speed = 1
 	tracer.rate = 0.1
 	tracer.blend_1 = Color.from_rgb(255, 255, 255)
-	tracer.blend_2 = tracer_color
+	tracer.blend_2 = ION_TRACER_COLOR
 	tracer.blend_rate = 0.2
 	tracer.image_alpha = 1.5
 	tracer.bm = 1
@@ -250,12 +257,12 @@ local ion_tracer, ion_tracer_info = CustomTracer.new(function(x1, y1, x2, y2)
 	local dist = gm.point_distance(x1, y1, x2, y2)
 	local dir = gm.point_direction(x1, y1, x2, y2)
 
-	tracer_particle:set_direction(dir, dir, 0, 0)
+	particleWispGTracer:set_direction(dir, dir, 0, 0)
 
 	local px = x1
 	local i = 0
 	while i < dist do
-		tracer_particle:create_colour(px, y1 + gm.random_range(-8, 8), tracer_color, 1)
+		particleWispGTracer:create_colour(px, y1 + gm.random_range(-8, 8), ION_TRACER_COLOR, 1)
 		px = px + gm.lengthdir_x(20, dir)
 		i = i + 20
 	end
@@ -269,8 +276,8 @@ executionerSecondary:onActivate(function(actor, skill)
 	actor:enter_state(stateExecutionerSecondary)
 end)
 executionerSecondary:onStep(function(actor, skill)
+	-- update ion burst's skill icon depending on how many rounds it has
 	local ion_rounds = skill.stock
-
 	local frame = 1
 
 	if ion_rounds == 0 then
@@ -322,7 +329,7 @@ stateExecutionerSecondary:onStep(function(actor, data)
 
 			local buff_shadow_clone = Buff.find("ror", "shadowClone")
 			for i=0, actor:buff_stack_count(buff_shadow_clone) do
-				local attack_info = actor:fire_bullet(actor.x, actor.y, 1000, dir, damage, nil, sprite_sparks2, ion_tracer).attack_info
+				local attack_info = actor:fire_bullet(actor.x, actor.y, 1000, dir, damage, nil, sprite_ion_sparks, ion_tracer).attack_info
 				attack_info:set_stun(1.0)
 				attack_info.climb = i * 8 * 1.35
 			end
