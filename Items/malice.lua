22529f63c51c9bd59ee0f993f2932b9fe5f7edd5
kris <kud.stanislav@gmail.com>
2025-03-19 22:25:17 +0500

    malice: improved feedback, new vfx

--- Items/malice.lua
+++ Items/malice.lua
@@ -1,26 +1,26 @@
-local sprite = Resources.sprite_load(NAMESPACE, "malice", path.combine(PATH, "Sprites/Items/malice.png"), 1, 16, 18)
+local item_sprite = Resources.sprite_load(NAMESPACE, "malice", path.combine(PATH, "Sprites/Items/malice.png"), 1, 16, 18)
+local spark_sprite = gm.constants.sSparks18s--Resources.sprite_load(NAMESPACE, "SparkMalice", path.combine(PATH, "Sprites/Items/Effects/malice.png"), 4, 8, 4)
 
 local RANGE_BASE = 90
 local RANGE_STACK = 21
 local DAMAGE_COEFFICIENT = 0.45
 
+local TRAVEL_TIME = 1 / 10 -- 10 frames
+local COLOR_BRIGHT = Color.from_rgb(188, 17, 255)
+
 local malice = Item.new(NAMESPACE, "malice")
-malice:set_sprite(sprite)
+local objEfMalice = Object.new(NAMESPACE, "EfMalice")
+local partMalice = Particle.new(NAMESPACE, "Malice")
+
+malice:set_sprite(item_sprite)
 malice:set_tier(Item.TIER.common)
 malice:set_loot_tags(Item.LOOT_TAG.category_damage)
 
-local objEfMalice = Object.new(NAMESPACE, "EfMalice")
-
-local partMalice = Particle.new(NAMESPACE, "Malice")
-
 partMalice:set_shape(Particle.SHAPE.disk)
---partMalice.set_colour3(Color.from_hex(0xBD69D6), Color.from_hex(0x784591), Color.from_hex(0x890060)) -- doesnt work !?!?
-GM.part_type_colour3(partMalice, Color.from_hex(0xBD69D6), Color.from_hex(0x784591), Color.from_hex(0x890060))
-partMalice:set_alpha2(1, 0)
-partMalice:set_size(0.09, 0.16, -0.005, 0)
-partMalice:set_speed(0, 2, -0.002, 0)
-partMalice:set_direction(0, 360, 0, 50)
-partMalice:set_life(3, 45)
+partMalice:set_life(15, 25)
+partMalice:set_size(0.08, 0.12, -0.005, 0)
+--partMalice:set_colour(Color.PURPLE) -- doesnt work !?!?
+gm.part_type_colour3(partMalice.value, COLOR_BRIGHT, Color.PURPLE, Color.PURPLE)
 
 malice:clear_callbacks()
 malice:onHitProc(function(actor, victim, stack, hit_info)
@@ -45,10 +45,6 @@ malice:onHitProc(function(actor, victim, stack, hit_info)
 
 		targets:delete(index)
 	end
-
-	local ef = GM.instance_create(victim.x, victim.y, gm.constants.oEfCircle)
-	ef.image_blend = Color.PURPLE
-	ef.rate = 12
 end)
 
 objEfMalice:clear_callbacks()
@@ -58,7 +54,8 @@ objEfMalice:onCreate(function(self)
 	self.parent = -4
 	self.target = -4
 
-	self.speed = 7
+	self.fract = 0
+	self.direction = math.random(360)
 
 	self:instance_sync()
 end)
@@ -66,34 +63,40 @@ objEfMalice:onStep(function(self)
 	if not Instance.exists(self.target) then self:destroy() return end
 	if not Instance.exists(self.parent) then self:destroy() return end
 
-	local x, y = self.x, self.y
+	self.fract = self.fract + TRAVEL_TIME
+
 	local target, parent = self.target, self.parent
 	local tx, ty = target.x, target.y
 
-	partMalice:create(x, y, 1, Particle.SYSTEM.above)
+	local coff = math.sin(self.fract * math.pi)
+	self.x = gm.lerp(self.xstart, tx, self.fract) + gm.lengthdir_x(16, self.direction) * coff
+	self.y = gm.lerp(self.ystart, ty, self.fract) + gm.lengthdir_y(16, self.direction) * coff
 
-	self.direction = gm.point_direction(x, y, tx, ty)
+	local dx, dy = self.x - self.xprevious, self.y - self.yprevious
 
-	if gm.point_distance(x, y, tx, ty) < 8 then
+	for i=0, 2 do
+		local f = i / 3
+		partMalice:create(self.x - dx * f, self.y - dy * f, 1, Particle.SYSTEM.above)
+	end
+
+	if self.fract + TRAVEL_TIME > 1 then
 		if gm._mod_net_isHost() then
-			local atk = self.parent:fire_direct(target, 1, self.direction, x, y, gm.constants.sSparks6, false).attack_info
+			local dir = gm.point_direction(self.xprevious, self.yprevious, self.x, self.y)
+			local atk = self.parent:fire_direct(target, 1, dir, self.x, self.y, spark_sprite, false).attack_info
 			atk.damage = self.damage
 			atk.critical = self.critical
-			atk.damage_color = Color.PURPLE
+			atk.damage_color = COLOR_BRIGHT
 		end
 
 		self:destroy()
 	end
 end)
 
--- clients dont need to know about the damage
 objEfMalice:onSerialize(function(self, buffer)
-	--buffer:write_uint(self.damage)
 	buffer:write_instance(self.parent)
 	buffer:write_instance(self.target)
 end)
 objEfMalice:onDeserialize(function(self, buffer)
-	--self.damage = buffer:read_uint()
 	self.parent = buffer:read_instance()
 	self.target = buffer:read_instance()
 end)
