772ad762794e037338665a7bbbafce9ccdcf6bae
kris <kud.stanislav@gmail.com>
2025-03-19 10:08:33 +0500

    man-o'-war: oops forgot effect color teehee uwu

--- Items/manOWar.lua
+++ Items/manOWar.lua
@@ -1,5 +1,7 @@
 local item_sprite = Resources.sprite_load(NAMESPACE, "ManOWar", path.combine(PATH, "Sprites/Items/manOWar.png"), 1, 16, 18)
 
+local EFFECT_COLOR = Color.from_hex(0xC6AAFF)
+
 local manOWar = Item.new(NAMESPACE, "manOWar")
 manOWar:set_sprite(item_sprite)
 manOWar:set_tier(Item.TIER.uncommon)
@@ -11,6 +13,7 @@ manOWar:onKillProc(function(killer, victim, stack)
 	lightning.team = killer.team
 	lightning.damage = killer.damage * (0.6 + 0.4 * stack)
 	lightning.bounce = 2
+	lightning.blend = EFFECT_COLOR
 
 	gm.sound_play_at(gm.constants.wChainLightning, 1.1 + math.random() * 0.2, 0.5, victim.x, victim.y)
 end)
