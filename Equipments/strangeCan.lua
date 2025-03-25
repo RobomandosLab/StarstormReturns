eb9a894d2b56f8aded6adf5743ccc7b8c892cd83
kris <kud.stanislav@gmail.com>
2025-03-26 01:49:39 +0500

    strange can: stuff

--- Equipments/strangeCan.lua
+++ Equipments/strangeCan.lua
@@ -1,60 +1,62 @@
-local item_sprite = Resources.sprite_load(NAMESPACE, "StrangeCan", path.combine(PATH, "Sprites/Equipments/strangeCan.png"), 2, 15, 15)
-local buff_sprite = Resources.sprite_load(NAMESPACE, "BuffIntoxication", path.combine(PATH, "Sprites/Buffs/intoxication.png"), 1, 11, 12)
+local sprite_item = Resources.sprite_load(NAMESPACE, "StrangeCan", path.combine(PATH, "Sprites/Equipments/strangeCan.png"), 2, 15, 15)
+local sprite_projectile = Resources.sprite_load(NAMESPACE, "EfStrangeCan", path.combine(PATH, "Sprites/Equipments/effects/strangeCan.png"), 1, 12, 7)
+local sprite_explosion = Resources.sprite_load(NAMESPACE, "EfStrangeCanExplosion", path.combine(PATH, "Sprites/Equipments/effects/strangeCanExplosion.png"), 5, 48, 38)
+local sprite_buff = Resources.sprite_load(NAMESPACE, "BuffIntoxication", path.combine(PATH, "Sprites/Buffs/intoxication.png"), 1, 11, 12)
 
-local INTOXICATION_COLOR = Color.from_rgb(62, 196, 85)
+local INTOXICATION_COLOR = Color.from_rgb(126, 182, 134)
 local INTOXICATION_RADIUS = 96
 
 local sound = Resources.sfx_load(NAMESPACE, "IntoxicateApply", path.combine(PATH, "Sounds/Items/strangeCan.ogg"))
 
 local strangeCan = Equipment.new(NAMESPACE, "strangeCan")
-strangeCan:set_sprite(item_sprite)
-strangeCan.cooldown = 45 * 60
+strangeCan:set_sprite(sprite_item)
+strangeCan:set_cooldown(45)
 
 local objTossedCan = Object.new(NAMESPACE, "EfCan")
-objTossedCan.obj_sprite = gm.constants.sEfDynamite
+objTossedCan.obj_sprite = sprite_projectile
+objTossedCan.obj_depth = -10
 
 local buffIntoxication = Buff.new(NAMESPACE, "intoxication")
 buffIntoxication.show_icon = true
-buffIntoxication.icon_sprite = buff_sprite
+buffIntoxication.icon_sprite = sprite_buff
 buffIntoxication.is_debuff = true
 
-local particlePoison = Particle.find("ror", "Poison")
+local particleRadioactive = Particle.find("ror", "Radioactive")
 
 strangeCan:clear_callbacks()
 strangeCan:onUse(function(actor, embryo)
-	local obj = objTossedCan:create(actor.x, actor.y)
-	obj.hspeed = 5 * actor.image_xscale
-	obj.vspeed = -2
+	local obj = objTossedCan:create(actor.x + 8 * actor.image_xscale, actor.y-6)
+	obj.direction = 90 - actor.image_xscale * 60
+	obj.speed = 6
 	obj.team = actor.team
-	if embryo then
-		obj.radius = obj.radius * 2
-	end
+	obj.embryo = embryo
 end)
 
 objTossedCan:clear_callbacks()
 objTossedCan:onCreate(function(self)
-	self.image_angle = math.random(360)
-	self.gravity = 0.2
+	self.gravity = 0.3
 	self.team = 1
-	self.radius = INTOXICATION_RADIUS
-	self.image_blend = INTOXICATION_COLOR
+	self.embryo = false
 end)
 objTossedCan:onStep(function(self)
 	self.image_angle = self.image_angle + 8
-	particlePoison:create_color(self.x, self.y, INTOXICATION_COLOR, 1)
+	particleRadioactive:create_color(self.x, self.y, INTOXICATION_COLOR, 1)
 
 	if self:is_colliding(gm.constants.pBlock) then
 		local ef = GM.instance_create(self.x, self.y, gm.constants.oEfExplosion)
-		ef.sprite_index = gm.constants.sEngiGrenadeExplosion
-		ef.image_xscale = 2
-		ef.image_yscale = 2
+		ef.sprite_index = sprite_explosion
 
 		self:sound_play(gm.constants.wClayDeath, 1, 1.5)
 		self:sound_play(gm.constants.wArtiShoot3_2, 0.5, 0.9 + math.random() * 0.1)
 		self:screen_shake(7)
 
-		local victims = List.wrap(self:find_characters_circle(self.x, self.y, self.radius, false, self.team))
+		local radius = INTOXICATION_RADIUS
+		if gm.bool(self.embryo) then
+			-- this kinda sucks... eh?
+			radius = radius * 2
+		end
 
+		local victims = List.wrap(self:find_characters_circle(self.x, self.y, radius, false, self.team))
 		for _, victim in ipairs(victims) do
 			victim:buff_apply(buffIntoxication, 7 * 60)
 		end
@@ -74,14 +76,14 @@ buffIntoxication:onPostStep(function(actor, stack)
 
 	if data.intoxication_timer >= 90 then
 		data.intoxication_timer = 0
-		local time_left = actor:get_buff_time(actor, buffIntoxication)
+		local time_left = GM.get_buff_time(actor, buffIntoxication)
 		if time_left then
 			gm.instance_create(actor.x, actor.y, gm.constants.oBugGuts)
 			gm.instance_create(actor.x, actor.y, gm.constants.oBugGuts)
 
 			if gm._mod_net_isHost() then
 				local dmg = math.ceil(actor.hp * 0.03 * time_left / 60)
-				actor:damage_inflict(actor, dmg, 0, -4, actor.x, actor.y, dmg, 1, INTOXICATION_COLOR)
+				gm.damage_inflict(actor.id, dmg, 0, -4, actor.x, actor.y, dmg, 1, INTOXICATION_COLOR)
 			end
 		end
 	end
