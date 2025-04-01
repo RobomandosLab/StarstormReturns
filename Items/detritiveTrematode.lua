443d4f80205df31ccdbee3ae358d477b607cf6be
kris <kud.stanislav@gmail.com>
2025-04-01 08:15:08 +0500

    detritive trematode: slight rework

--- Items/detritiveTrematode.lua
+++ Items/detritiveTrematode.lua
@@ -1,27 +1,23 @@
 local item_sprite = Resources.sprite_load(NAMESPACE, "DetritiveTrematode", path.combine(PATH, "Sprites/Items/detritiveTrematode.png"), 1, 16, 20)
 local buff_sprite = Resources.sprite_load(NAMESPACE, "BuffDisease", path.combine(PATH, "Sprites/Buffs/disease.png"), 1, 8, 8)
 
+local DAMAGE_COLOR = Color.from_rgb(187, 211, 91)
+
 local detritiveTrematode = Item.new(NAMESPACE, "detritiveTrematode")
 detritiveTrematode:set_sprite(item_sprite)
 detritiveTrematode:set_tier(Item.TIER.common)
 detritiveTrematode:set_loot_tags(Item.LOOT_TAG.category_damage)
 
+local detritiveTrematodeID = detritiveTrematode.value
+
 local buffDisease = Buff.new(NAMESPACE, "disease")
+buffDisease.icon_sprite = buff_sprite
+buffDisease.is_debuff = true
+buffDisease.is_timed = false
 
 detritiveTrematode:clear_callbacks()
-detritiveTrematode:onAttackHit(function(actor, victim, stack, hit_info)
-	victim = GM.attack_collision_resolve(victim)
-
-	if not Instance.exists(victim) then return end
-
-	--local threshold = ((0.9 + stack / 1.2) * 0.033)
-	threshold = 0.05 + 0.05 * stack
-	threshold = GM.actor_get_maxhp_total(victim) * threshold
-	if victim.hp - hit_info.damage < threshold then
-		victim:buff_apply(buffDisease, 7 * 60)
-	end
-end)
-
+-- this effect honestly looks ugly ..?? idk
+--[[
 local effect_blend_color = Color.from_rgb(128, 128, 0)
 detritiveTrematode:onPostDraw(function(actor, stack)
 	local preserve_alpha, preserve_blend = actor.image_alpha, actor.image_blend
@@ -33,25 +29,35 @@ detritiveTrematode:onPostDraw(function(actor, stack)
 	actor.image_alpha = preserve_alpha
 	actor.image_blend = preserve_blend
 end)
-
-buffDisease.icon_sprite = buff_sprite
-buffDisease.is_debuff = true
-
-local disease_damage_color = Color.from_rgb(187, 211, 91)
+--]]
 
 buffDisease:clear_callbacks()
 buffDisease:onApply(function(actor, stack)
-	actor:get_data().disease_timer = 0
+	actor:sound_play(gm.constants.wBrambleShoot1, 0.8, 0.4 + math.random() * 0.2)
 end)
 buffDisease:onPostStep(function(actor, stack)
 	if gm._mod_net_isClient() then return end
+	if Global._current_frame % 30 == 0 then
+		local dmg = actor.__ssr_trematode_damage or 1
+		gm.damage_inflict(actor.id, dmg, 0, -4, actor.x, actor.y, dmg, 1, DAMAGE_COLOR)
+	end
+end)
 
-	local data = actor:get_data()
-	if data.disease_timer >= 30 then
-		data.disease_timer = 0
-		local dmg = actor.maxhp * 0.01
-		actor:damage_inflict(actor, dmg, 0, -4, actor.x, actor.y, dmg, 1, disease_damage_color)
-	else
-		data.disease_timer = data.disease_timer + 1
+-- can't use attack callbacks because they run before the actual hp is decreased, so just hook this instead
+gm.post_script_hook(gm.constants.damager_hit_process, function(self, other, result, args)
+	local _hit_info = args[1].value
+	local _target = args[2].value
+	local _parent = _hit_info.parent
+
+	if not Instance.exists(_target) or _target.hp <= 0 then return end
+
+	local count = gm.item_count(_parent or -4, detritiveTrematodeID) or 0
+	if count > 0 then
+		local threshold = 0.1 + 0.05 * count
+
+		if _target.hp <= _target.maxhp * threshold then
+			gm.apply_buff(_target, buffDisease.value, 60)
+			_target.__ssr_trematode_damage = _parent.damage * 0.5
+		end
 	end
 end)
