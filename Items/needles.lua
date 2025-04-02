d65f5885d90df0a21ea8b82b3f85051d9e8de0d0
kris <kud.stanislav@gmail.com>
2025-04-02 10:23:56 +0500

    needles: don't allow extending mark duration

--- Items/needles.lua
+++ Items/needles.lua
@@ -15,7 +15,7 @@ buffNeedlesID = buffNeedles.value
 
 needles:clear_callbacks()
 needles:onHitProc(function(actor, victim, stack, hit_info)
-	if math.random() <= 0.01 + stack * 0.02 then
+	if math.random() <= 0.01 + stack * 0.02 and victim:buff_stack_count(buffNeedles) == 0 then
 		victim:buff_apply(buffNeedles, 190)
 	end
 end)
