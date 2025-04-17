70863ee13bf8eb102da459adb5b643fdb2b220a8
Azuline <azulineSkye@gmail.com>
2025-04-18 02:33:58 +0700

    gathering (attempt at) optimisation (futile)

--- Artifacts/gathering.lua
+++ Artifacts/gathering.lua
@@ -8,38 +8,76 @@ local pickup = Resources.sprite_load(NAMESPACE, "ArtifactOfGatheringPickup", pat
 local gathering = Artifact.new(NAMESPACE, "gathering")
 gathering:set_sprites(loadout, pickup)
 
-Callback.add(Callback.TYPE.postStep, "Gathering", function()
+-- OLD CODE THAT DOESNT USE post_code_execute BUT RUNS LIKE SHIT
+-- TBH post_code_execute CODE STILL RUNS LIKE SHIT JUST A BIT LESS SO
+-- GOD IM HOPING RETURNS API COMES OUT SOONER THAN LATER
+
+--Callback.add(Callback.TYPE.postStep, "GatheringCoins", function()
+--	if not gathering.active then return end
+--	
+--	for _, gold in ipairs(Instance.find_all(gm.constants.oEfGold)) do
+--		local data = gold:get_data()
+
+--		if not data.gatheringGold then
+--			data.gatheringGold = true
+--			gold.value.value = gold.value.value * 2 -- we have to use value.value because value is already a thing that returns a cinstance in rmt
+--			data.life = 1000
+--		end
+
+--		if gold:alarm_get(1) ~= -1 then -- alarm 1 controls whether the gold is homing in on the player or not
+--			gold:alarm_set(1, -1) -- setting it to -1 if it isnt already -1 makes it not home in
+--		end
+
+--		-- alarm 2 controls whether the gold is being picked up
+--		-- whether this alarm is active or not is based on alarm 1, and since we stopped alarm 1 this wont work on its own
+--		if gold:alarm_get(2) == -1 and gold:is_colliding(gm.constants.oP) then -- solution? activate the alarm manually when a coin is colliding with a player object
+--			gold:alarm_set(2, 3)
+--		end
+
+--		if (data.life or 0) > 0 then
+--			data.life = data.life - 1
+--			if data.life < 100 and data.life % 10 < 2 then -- make it blink when about to disappear!!
+--				gold.visible = false
+--			else
+--				gold.visible = true
+--			end
+--		else
+--			gold:destroy()
+--		end
+--	end
+--end)
+
+gm.post_code_execute("gml_Object_oEfGold_Create_0", function(self, other)
 	if not gathering.active then return end
 	
-	for _, gold in ipairs(Instance.find_all(gm.constants.oEfGold)) do
-		local data = gold:get_data()
-		if not data.gatheringGold then
-			data.gatheringGold = true
-			gold.value.value = gold.value.value * 2 -- we have to use value.value because value is already a thing that returns a cinstance in rmt
-			data.life = 1000
-		end
-		if gold:alarm_get(1) ~= -1 then -- alarm 1 controls whether the gold is homing in on the player or not
-			gold:alarm_set(1, -1) -- setting it to -1 if it isnt already -1 makes it not home in
-		end
-		-- alarm 2 controls whether the gold is being picked up
-		-- whether this alarm is active or not is based on alarm 1, and since we stopped alarm 1 this wont work on its own
-		if gold:alarm_get(2) == -1 and gold:is_colliding(gm.constants.oP) then -- solution? activate the alarm manually when a coin is colliding with a player object
-			gold:alarm_set(2, 3)
-		end
-		local n = 0
-		while gold:is_colliding(gm.constants.pBlock, gold.x, gold.y) and n < 20 do -- if a coin is inside collision we push it up a little so it doesnt get stuck
-			gold.y = gold.y - 1
-			n = n + 1
-		end
-		if (data.life or 0) > 0 then
-			data.life = data.life - 1
-			if data.life < 100 and data.life % 10 < 2 then -- make it blink when about to dissapear!!
-				gold.visible = false
-			else
-				gold.visible = true
-			end
+	local gold = Instance.wrap(self)
+	gold.value.value = gold.value.value * 2 -- we have to use value.value because value is already a thing that returns a cinstance in rmt
+	gold.life = 1000
+end)
+
+gm.pre_code_execute("gml_Object_oEfGold_Step_2", function(self, other)
+	if not gathering.active then return end
+	
+	local gold = Instance.wrap(self)
+	
+	if gold:alarm_get(1) ~= -1 then -- alarm 1 controls whether the gold is homing in on the player or not
+		gold:alarm_set(1, -1) -- setting it to -1 if it isnt already -1 makes it not home in
+	end
+	
+	-- alarm 2 controls whether the gold is being picked up
+	-- whether this alarm is active or not is based on alarm 1, and since we stopped alarm 1 this wont work on its own
+	if gold:alarm_get(2) == -1 and gold:is_colliding(gm.constants.oP) then -- solution? activate the alarm manually when a coin is colliding with a player object
+		gold:alarm_set(2, 3)
+	end
+	
+	if (gold.life or 0) > 0 then
+		gold.life = gold.life - 1
+		if gold.life < 100 and gold.life % 10 < 2 then -- make it blink when about to disappear!!
+			gold.visible = false
 		else
-			gold:destroy()
+			gold.visible = true
 		end
+	else
+		gold:destroy()
 	end
 end)
