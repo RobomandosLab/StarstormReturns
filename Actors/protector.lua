5f64c5414cae0b1a3075d9c71af54170d4dc1423
Azuline <azulineSkye@gmail.com>
2025-04-23 18:21:59 +0700

    test

--- Actors/protector.lua
+++ Actors/protector.lua
@@ -1,4 +1,4 @@
--- todo add comments later
+-- todo add comments NOW
 local SPRITE_PATH = path.combine(PATH, "Sprites/Actors/Protector")
 local SOUND_PATH = path.combine(PATH, "Sounds/Actors/Gatekeeper")
 
