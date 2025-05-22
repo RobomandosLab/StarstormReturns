835b6fbb1b0afbd5f60aa19ec9d5395d582d8444
Azuline <azulineSkye@gmail.com>
2025-05-23 00:31:36 +0700

    secondary

--- Survivors/mule.lua
+++ Survivors/mule.lua
@@ -21,6 +21,7 @@ local sprite_decoy			= Resources.sprite_load(NAMESPACE, "MuleDecoy", path.combin
 local sprite_shoot1a		= Resources.sprite_load(NAMESPACE, "MuleShoot1_1", path.combine(SPRITE_PATH, "shoot1_1.png"), 6, 21, 34)
 local sprite_shoot1b		= Resources.sprite_load(NAMESPACE, "MuleShoot1_2", path.combine(SPRITE_PATH, "shoot1_2.png"), 6, 21, 34)
 local sprite_shoot1c		= Resources.sprite_load(NAMESPACE, "MuleShoot1_33", path.combine(SPRITE_PATH, "shoot1_2.png"), 6, 21, 34)
+local sprite_shoot2			= Resources.sprite_load(NAMESPACE, "MuleShoot2", path.combine(SPRITE_PATH, "shoot2.png"), 7, 30, 33)
 local sprite_shoot1charge	= Resources.sprite_load(NAMESPACE, "MuleShoot1Charge", path.combine(SPRITE_PATH, "shoot1_charge.png"), 6, 21, 28)
 local sprite_shoot3			= Resources.sprite_load(NAMESPACE, "MuleShoot3", path.combine(SPRITE_PATH, "shoot3.png"), 15, 48, 33)
 local sprite_shoot4			= Resources.sprite_load(NAMESPACE, "MuleShoot4", path.combine(SPRITE_PATH, "shoot4.png"), 15, 23, 27)
@@ -29,6 +30,11 @@ local sprite_shoot4heal		= Resources.sprite_load(NAMESPACE, "MuleShoot4Heal", pa
 local sprite_shoot4			= Resources.sprite_load(NAMESPACE, "MuleShoot4", path.combine(SPRITE_PATH, "shoot4.png"), 15, 23, 27)
 local sprite_drone_idle		= Resources.sprite_load(NAMESPACE, "MulePlayerDroneIdle", path.combine(SPRITE_PATH, "playerdrone.png"), 5, 15, 13)
 local sprite_drone_shoot	= Resources.sprite_load(NAMESPACE, "MulePlayerDroneShoot", path.combine(SPRITE_PATH, "playerdroneshoot.png"), 5, 33, 13)
+local sprite_trap_debuff	= Resources.sprite_load(NAMESPACE, "MuleTrapDebuff", path.combine(SPRITE_PATH, "trap_debuff.png"), 8, 18, 18)
+local sprite_snare_debuff	= Resources.sprite_load(NAMESPACE, "MuleSnareDebuff", path.combine(SPRITE_PATH, "snare_debuff.png"), 1, 18, 12)
+local sprite_sparks1		= Resources.sprite_load(NAMESPACE, "MuleSparks1", path.combine(SPRITE_PATH, "sparks1.png"), 3, 10, 16)
+local sprite_sparks2		= Resources.sprite_load(NAMESPACE, "MuleSparks2", path.combine(SPRITE_PATH, "sparks2.png"), 4, 14, 10)
+local sprite_sparks3		= Resources.sprite_load(NAMESPACE, "MuleSparks3", path.combine(SPRITE_PATH, "sparks3.png"), 4, 8, 16)
 
 local sound_shoot1a			= Resources.sfx_load(NAMESPACE, "MuleShoot1a", path.combine(SOUND_PATH, "skill1a.ogg"))
 local sound_shoot1b			= Resources.sfx_load(NAMESPACE, "MuleShoot1b", path.combine(SOUND_PATH, "skill1b.ogg"))
@@ -83,7 +89,7 @@ mule:set_animations({
 	drone_shoot = sprite_drone_shoot,
 })
 
-mule:set_cape_offset(0, -14, 0, -8)
+mule:set_cape_offset(0, -14, 0, -18)
 mule:set_primary_color(Color.from_rgb(211,176,122))
 
 mule.sprite_loadout = sprite_loadout
@@ -96,6 +102,83 @@ mule.sprite_credits = sprite_credits
 mule:clear_callbacks()
 mule:onInit(function(actor)
 	actor:get_data().z_count = 0
+end)
+
+local snare = Buff.new(NAMESPACE, "muleSnare")
+snare.icon_sprite = sprite_snare_debuff
+snare.show_icon = true
+snare.is_debuff = true
+snare:clear_callbacks()
+
+snare:onApply(function(actor, stack)
+	actor.pHspeed = 0
+	if not GM.actor_is_boss(actor) then
+		actor.activity = 46
+		actor.pHspeed = 0
+	end
+end)
+
+snare:onPostStep(function(actor, stack)
+	actor.pHspeed = 0
+	if not GM.actor_is_boss(actor) then
+		actor.activity = 46
+		if actor.sprite_idle then
+			actor.sprite_index = actor.sprite_idle
+		end
+	end
+end)
+
+snare:onStatRecalc(function(actor, stack)
+	actor.pHmax = actor.pHmax - 4
+end)
+
+snare:onRemove(function(actor, stack)
+	if not GM.actor_is_boss(actor) then
+		actor:skill_util_reset_activity_state()
+	end
+end)
+
+local trap = Buff.new(NAMESPACE, "muleTrap")
+trap.icon_sprite = sprite_trap_debuff
+trap.icon_frame_speed = 0.25
+trap.show_icon = true
+trap.is_debuff = true
+trap:clear_callbacks()
+
+trap:onPostDraw(function(actor, stack)
+	for _, victim in ipairs(actor:get_data().trapped_enemies) do
+		if Instance.exists(victim) then
+			local parent = victim.trap_parent
+			if parent and Instance.exists(parent) then
+				if not (victim:get_data().trap_offset_a and victim:get_data().trap_offset_b) then
+					victim:get_data().trap_offset_a = math.random(-8, 8)
+					victim:get_data().trap_offset_b = victim.x + math.random(-16, 16)
+					
+					local yy = 0
+					while yy < 100 and victim:collision_point(victim.x, victim.y + yy, gm.constants.pBlock, true, false) == -4.0 do
+						yy = yy + 2
+					end
+					if yy < 100 and not victim:get_data().trap_offset_c then
+						victim:get_data().trap_offset_c = victim.y + yy
+					else
+						victim:get_data().trap_offset_c = nil
+					end
+				end
+				
+				gm.draw_set_alpha(0.8)
+				gm.draw_set_colour(Color.from_rgb(205, 205, 205))
+				gm.draw_line_width(victim.x, victim.y, parent.x, parent.y + victim:get_data().trap_offset_a, 3)
+				
+				if victim:get_data().trap_offset_c then
+					gm.draw_line_width(victim.x, victim.y, victim:get_data().trap_offset_b, victim:get_data().trap_offset_c, 3)
+				end
+			end
+		end
+	end
+end)
+
+trap:onRemove(function(actor, stack)
+	actor:get_data().trapped_enemies:destroy()
 end)
 
 local objWave = Object.new(NAMESPACE, "MuleShockwave")
@@ -278,7 +361,7 @@ statePrimary:onStep(function(actor, data)
 						attack_info = actor:fire_bullet(actor.x + 20 * actor.image_xscale, actor.y - 8, 700, actor:skill_util_facing_direction(), 10, 1, gm.constants.sSparks1, Attack_Info.TRACER.drill).attack_info
 						attack_info.climb = i * 8
 					else
-						local attack_info = actor:fire_explosion(actor.x + 50 * actor.image_xscale, actor.y, 120, 60, 10, nil, nil).attack_info
+						local attack_info = actor:fire_explosion(actor.x + 50 * actor.image_xscale, actor.y, 120, 60, 10, nil, sprite_sparks1).attack_info
 						attack_info.climb = i * 8
 						attack_info.knockback = attack_info.knockback + 1.5 * data.strength
 						attack_info.knockback_direction = actor.image_xscale
@@ -286,12 +369,14 @@ statePrimary:onStep(function(actor, data)
 					end
 				end
 			end
-			local buff_shadow_clone = Buff.find("ror", "shadowClone")
-			for i=0, actor:buff_stack_count(buff_shadow_clone) do
-				local wave = objWave:create(actor.x + i * 32 * actor.image_xscale, actor.y)
-				wave.direction = actor:skill_util_facing_direction()
-				wave.depth = wave.depth + i
-				wave.parent = actor
+			if not gm.bool(actor.free) then
+				local buff_shadow_clone = Buff.find("ror", "shadowClone")
+				for i=0, actor:buff_stack_count(buff_shadow_clone) do
+					local wave = objWave:create(actor.x + i * 32 * actor.image_xscale, actor.y)
+					wave.direction = actor:skill_util_facing_direction()
+					wave.depth = wave.depth + i
+					wave.parent = actor
+				end
 			end
 		elseif data.fired == 1 and actor.image_index >= 1 then
 			data.fired = 3
@@ -316,7 +401,7 @@ statePrimary:onStep(function(actor, data)
 						attack_info = actor:fire_bullet(actor.x + 20 * actor.image_xscale, actor.y - 8, 700, actor:skill_util_facing_direction(), dmg, 1, gm.constants.sSparks1, Attack_Info.TRACER.drill).attack_info
 						attack_info.climb = i * 8
 					else
-						local attack_info = actor:fire_explosion(actor.x + 30 * actor.image_xscale, actor.y, 80, 60, dmg, nil, nil).attack_info
+						local attack_info = actor:fire_explosion(actor.x + 30 * actor.image_xscale, actor.y, 80, 60, dmg, nil, sprite_sparks1).attack_info
 						attack_info.climb = i * 8
 						attack_info.knockback = attack_info.knockback + 1.5 * data.strength
 						attack_info.knockback_direction = actor.image_xscale
@@ -337,6 +422,72 @@ secondary:set_skill_icon(sprite_skills, 1)
 
 secondary.cooldown = 4 * 60
 secondary.damage = 1.25
+
+local stateSecondary = State.new(NAMESPACE, "muleSecondary")
+
+secondary:clear_callbacks()
+secondary:onActivate(function(actor)
+	actor:enter_state(stateSecondary)
+end)
+
+stateSecondary:clear_callbacks()
+stateSecondary:onEnter(function(actor, data)
+	actor.image_index = 0
+	data.fired = 0
+end)
+
+stateSecondary:onStep(function(actor, data)
+	actor:actor_animation_set(sprite_shoot2, 0.2)
+	actor:skill_util_fix_hspeed()
+	
+	if data.fired == 0 and actor.image_index >= 1 then
+		data.fired = 1
+		actor:sound_play(sound_shoot2a, 1, 0.9 + math.random() * 0.2)
+		if actor:is_authority() then
+			local buff_shadow_clone = Buff.find("ror", "shadowClone")
+			for i=0, actor:buff_stack_count(buff_shadow_clone) do
+				local attack_info = actor:fire_bullet(actor.x, actor.y - 4, 1300, actor:skill_util_facing_direction(), 1.25, nil, sprite_sparks3).attack_info
+				attack_info.climb = i * 8
+				attack_info.mule_immobilize = 1
+			end
+		end
+	end
+	
+	actor:skill_util_exit_state_on_anim_end()
+end)
+
+Callback.add(Callback.TYPE.onAttackHit, "muleInflictImmobilize", function(hit_info)
+	if hit_info.attack_info.mule_immobilize == 1 then
+		actor = hit_info.target
+		GM.apply_buff(actor, trap, 3 * 60, 1)
+		
+		actor:sound_play(sound_shoot2b, 1, 0.9 + math.random() * 0.2)
+	
+		local victims = List.new()
+	
+		actor:get_data().trapped_enemies = List.new()
+		actor:get_data().trapped_enemies:clear()
+	
+		actor:collision_ellipse_list(actor.x - 280, actor.y - 200, actor.x + 280, actor.y + 200, gm.constants.pActor, false, false, victims, true)
+		local count = 0
+		for _, victim in ipairs(victims) do
+			if victim.team == actor.team then
+				GM.apply_buff(victim, snare, 3 * 60, 1)
+				victim.trap_parent = actor
+				victim:get_data().trap_offset_a = nil
+				victim:get_data().trap_offset_b = nil
+				victim:get_data().trap_offset_c = nil
+				actor:get_data().trapped_enemies:add(victim.value)
+				count = count + 1
+				if count > 4 then
+					break
+				end
+			end
+		end
+
+		victims:destroy()
+	end
+end)
 
 -- TORQUE CALIBRATION
 utility:set_skill_icon(sprite_skills, 2)
@@ -383,7 +534,7 @@ stateUtility:onStep(function(actor, data)
 
 			local buff_shadow_clone = Buff.find("ror", "shadowClone")
 			for i = 0, actor:buff_stack_count(buff_shadow_clone) do
-				local attack_info = actor:fire_explosion(actor.x, actor.y, 160, 100, damage, nil).attack_info
+				local attack_info = actor:fire_explosion(actor.x, actor.y, 160, 100, damage, nil, sprite_sparks2).attack_info
 				attack_info.climb = i * 8
 				attack_info.knockback = 5
 				attack_info.knockback_direction = actor.image_xscale
