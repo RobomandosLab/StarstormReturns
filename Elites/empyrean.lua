0e9d6f4626ecfcaaca3bddb6d1a3a348048e0da6
Azuline <azulineSkye@gmail.com>
2025-08-04 21:55:48 +0700

    Prismatic Leviathan

--- Elites/empyrean.lua
+++ Elites/empyrean.lua
@@ -1,16 +1,19 @@
+-- ugh ..... ugmhgg .... it appreas it is me azuline again .....
+-- this elite is gay af.... just like me ... so it was only appropriate that i would do it ....
+
 local SPRITE_PATH = path.combine(PATH, "Sprites/Elites/Empyrean")
 local SOUND_PATH = path.combine(PATH, "Sounds/Elites/Empyrean")
 
 local sprite_icon = Resources.sprite_load(NAMESPACE, "EliteIconEmpyrean", path.combine(SPRITE_PATH, "icon.png"), 1, 24, 15)
-local sprite_palette = Resources.sprite_load(NAMESPACE, "ElitePaletteEmpyrean", path.combine(SPRITE_PATH, "palette.png"))
 local splash_sprite = Resources.sprite_load(NAMESPACE, "ParticleEmpyreanSpawnSplash", path.combine(SPRITE_PATH, "splash.png"), 6)
 local beam_sprite = Resources.sprite_load(NAMESPACE, "ParticleEmpyreanSpawnBeam", path.combine(SPRITE_PATH, "beam.png"), 11)
+local star_sprite = Resources.sprite_load(NAMESPACE, "EmpyreanWormStar", path.combine(SPRITE_PATH, "star.png"), 8, 16, 16)
 
 local gotanythingsharp = Resources.sfx_load(NAMESPACE, "EmpyreanSpawn", path.combine(SOUND_PATH, "beam.ogg"))
 
 local empy = Elite.new(NAMESPACE, "empyrean")
 empy.healthbar_icon = sprite_icon
-empy.palette = sprite_palette
+empy.palette = gm.constants.sElitePaletteDummy
 empy.blend_col = Color.WHITE
 
 GM.elite_generate_palettes()
@@ -34,60 +37,96 @@ local rainbowspark = Particle.new(NAMESPACE, "EmpyreanRainbowSpark")
 rainbowspark:set_shape(Particle.SHAPE.line)
 rainbowspark:set_blend(true)
 rainbowspark:set_alpha3(1, 1, 0)
-rainbowspark:set_size(0, 2, 0, 0.02)
+rainbowspark:set_size(0, 1, 0, 0.01)
 rainbowspark:set_orientation(0, 0, 0, 0, true)
 rainbowspark:set_speed(0, 4, -0.04, 0.2)
 rainbowspark:set_direction(0, 180, 0, 10)
 rainbowspark:set_scale(0.1, 0.1)
 rainbowspark:set_life(20, 100)
 
-
-
--- PUT ASPECTS HERE --
-function ssr_give_empyrean_aspects(actor)
-	-- VANILLA ELITES --
-
-	-- volatile
-	actor:item_give(Item.find("ror-eliteOrbExplosiveShot"))
-	actor:item_give(Item.find("ror-elitePassiveVolatile"))
-	
-	-- overloading effects
-	actor:item_give(Item.find("ror-eliteOrbLightning"))
-	actor:item_give(Item.find("ror-elitePassiveOverloading"))
-	
-	-- leeching effects
-	actor:item_give(Item.find("ror-elitePassiveLeeching"))
-	actor:item_give(Item.find("ror-eliteOrbLifesteal"))
-	
-	-- frenzied effects
-	actor:item_give(Item.find("ror-eliteOrbAttackSpeed"))
-	actor:item_give(Item.find("ror-eliteOrbMoveSpeed"))
-	actor:item_give(Item.find("ror-elitePassiveTeleport"))
-	
-	-- blazing effects
-	actor:item_give(Item.find("ror-eliteOrbFireTrail"))
-	
-	-- STARSTORM ELITES --
-	
-	-- poison effects
-	actor:item_give(Item.find("ssr-eliteOrbPoison"))
-end
+local startrail = Particle.new(NAMESPACE, "EmpyreanWormStarTrail")
+startrail:set_sprite(star_sprite, true, true, false)
+startrail:set_alpha2(0.75, 0)
+startrail:set_life(5, 5)
 
 local empyorb = Item.new(NAMESPACE, "eliteOrbEmpyrean", true)
 empyorb.is_hidden = true
 
+local teleorb = Item.new(NAMESPACE, "elitePassiveTeleportEmpyrean", true)
+teleorb.is_hidden = true
+
+-- PUT ASPECTS HERE --
+-- format is {"namespace-identifier", stack}
+local aspects = {
+	-- VANILLA --
+	
+	-- volatile
+	{"ror-eliteOrbExplosiveShot", 2},
+	{"ror-elitePassiveVolatile", 1},
+	
+	-- overloading
+	{"ror-eliteOrbLightning", 1},
+	{"ror-elitePassiveOverloading", 1},
+	
+	-- leeching
+	{"ror-elitePassiveLeeching", 1},
+	{"ror-eliteOrbLifesteal", 7},
+	
+	-- frenzied (minus their teleporting orb)
+	{"ror-eliteOrbAttackSpeed", 3},
+	{"ror-eliteOrbMoveSpeed", 5},
+	
+	-- blazing
+	{"ror-eliteOrbFireTrail", 5},
+	
+	
+	
+	-- STARSTORM --
+	
+	-- poison
+	{"ssr-eliteOrbPoison", 1}
+}
+function ssr_give_empyrean_aspects(actor)
+	for _, aspect in ipairs(aspects) do
+		local item = Item.find(aspect[1])
+		
+		-- if the elite already has these orbs, remove them
+		if actor:item_stack_count(item) > 0 then
+			actor:item_remove(item, actor:item_stack_count(item))
+		end
+ 		
+		-- give the orbs
+		actor:item_give(item, aspect[2])
+	end
+	
+	for id, stack in ipairs(actor.inventory_item_stack) do
+		if stack > 0 then
+			local item = Item.wrap(id - 1) -- subtract by one cuz lua tables start at 1 instead of 0
+		end
+	end
+end
+
 empy:clear_callbacks()
 empy:onApply(function(actor)
-	actor:item_give(empyorb)
+	actor:item_give(empyorb) -- applies most empyrean effects
+	actor:item_give(teleorb) -- makes it teleport to the player from any location
+	
+	-- stat changes are multiplicative with normal elite stat changes
+	-- so max hp for example will be base * 12 * 2.8 = 33.6x total multiplier
 	
 	-- giga health
-	actor.maxhp_base = actor.maxhp_base * 10
+	actor.maxhp_base = actor.maxhp_base * 12
 	actor.hp = actor.maxhp
 	
-	 -- giga gold and exp
+	-- giga gold and exp
 	if actor.exp_worth then
-		actor.exp_worth = math.min(actor.exp_worth * 15, 5000000)
+		actor.exp_worth = actor.exp_worth * 30 -- totals to 60x
 	end
+	
+	-- immune to stun, knockback and fall damage
+	actor.knockback_immune = true
+	actor.stun_immune = true
+	actor.fall_immune = true
 	
 	-- make maxhp_base modification take effect
 	GM.actor_queue_dirty(actor) 
@@ -95,9 +134,13 @@ end)
 
 empyorb:clear_callbacks()
 empyorb:onAcquire(function(actor, stack)
+	-- move the actor to the ground
+	actor:move_contact_solid(270, -1)
+	
+	-- apply screenshake
+	actor:screen_shake(4)
 	
 	-- play the spawn sound
-	actor:sound_play(gm.constants.wWormExplosion, 1, 0.6)
 	actor:sound_play(gotanythingsharp, 1, 1)
 	
 	-- start the beam
@@ -133,7 +176,7 @@ empyorb:onPostDraw(function(actor, stack)
 		elseif actor:get_data().empy_beam > 168 then
 			width = width * ((180 - actor:get_data().empy_beam) ^ 3) / 1000 -- make the beam widen when it appears
 		elseif actor:get_data().empy_beam > 165 then
-			width = width * (1.3 - ((169 - actor:get_data().empy_beam) / 10)) -- make the beam widen when it appears
+			width = width * (1.3 - ((169 - actor:get_data().empy_beam) / 10)) -- make the beam shrink back to its normal size after it widens
 		else
 			if math.random() >= 0.5 then
 				-- create the black particles around the enemy
@@ -146,39 +189,38 @@ empyorb:onPostDraw(function(actor, stack)
 				end
 				local ori = 180 + math.random(-45, 45) - 45 * side
 				splash:set_orientation(ori, ori, 0, 0, false)
-				splash:create_color(actor.x + (width + math.random(-3, 3)) * side, actor.bbox_bottom, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
+				splash:create_color(actor.x + (width + math.random(3)) * side, actor.bbox_bottom, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
 			end
 		end
 		
 		if actor:get_data().empy_beam > 15 and actor:get_data().empy_beam <= 170 then
 			-- create the beam particles that touch the ground
-			beam:create_color(actor.x - width - math.random(3), actor.y - 72, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
-			beam:create_color(actor.x + width + math.random(3), actor.y - 72, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
+			beam:create_color(actor.x - width - math.random(3), actor.y - 76, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
+			beam:create_color(actor.x + width + math.random(3), actor.y - 76, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
 			
 			-- create the beam particles around the beam
-			for i = 0, 15 do
-				beam:create_color(actor.x - width - math.random(3), actor.y - math.random(80, 1152), Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
-				beam:create_color(actor.x + width + math.random(3), actor.y - math.random(80, 1152), Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
-			end
-			
-			if gm.inside_view(actor.x, actor.y - 1152) == 1 then
-				for i = 0, 7 do
-					beam:create_color(actor.x - width - math.random(-3, 3), actor.y - math.random(1152, 1728), Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
-					beam:create_color(actor.x + width + math.random(-3, 3), actor.y - math.random(1152, 1728), Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
+			for i = 0, 22 do
+				local rnd = math.random(80, 1728)
+				if gm.inside_view(actor.x, actor.y - rnd) == 1 then
+					beam:create_color(actor.x - width - math.random(3), actor.y - math.random(80, 1152), Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
+					beam:create_color(actor.x + width + math.random(3), actor.y - math.random(80, 1152), Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
 				end
 			end
 		end
 		
 		-- draw the beam
-		gm.draw_set_alpha(1)
+		gm.draw_set_alpha(actor:get_data()._imalpha)
 		gm.draw_set_color(Color.WHITE)
 		gm.draw_rectangle(actor.x - width, 0, actor.x + width, actor.bbox_bottom, false)
 		
 		-- make it black and move
 		gm.draw_sprite_ext(actor.sprite_index, actor.image_index, actor.x, silhouette_y, actor.image_xscale, actor.image_yscale, actor.image_angle, Color.BLACK, math.min(1, ((180 - actor:get_data().empy_beam) ^ 3) / 1000))
+		gm.draw_set_alpha(1)
 	else
 		-- make it rainbow
-		gm.draw_sprite_ext(actor.sprite_index, actor.image_index, actor.x, actor.y, actor.image_xscale, actor.image_yscale, actor.image_angle, Color.from_hsv(Global._current_frame % 360, 100, 100), 1)
+		if actor.object_index ~= gm.constants.oWorm then
+			gm.draw_sprite_ext(actor.sprite_index, actor.image_index, actor.x, actor.y, actor.image_xscale, actor.image_yscale, actor.image_angle, Color.from_hsv(Global._current_frame % 360, 100, 100), actor.image_alpha)
+		end
 	end
 end)
 
@@ -191,28 +233,47 @@ gm.pre_script_hook(gm.constants.draw_hp_bar, function(self, other, result, args)
 	end
 end)
 
+local guarded = false
+
+-- disable fall damage for the elite to prevent cheese
+gm.pre_script_hook(gm.constants.actor_phy_on_landed, function(self, other, result, args)
+    local real_self = Instance.wrap(self)
+    if not gm.bool(self.invincible) and real_self.fall_immune == true then
+        self.invincible = 1
+        guarded = true
+    end
+end)
+
+gm.post_script_hook(gm.constants.actor_phy_on_landed, function(self, other, result, args)
+    if guarded then
+        self.invincible = 0
+        guarded = false
+    end
+end)
+
 empyorb:onPostStep(function(actor, stack)
 	if actor:get_data().empy_beam > 0 and actor:get_data().empy_beam_over == 0 then -- freeze the enemy
-		-- store the enemy's max speeds so you can restore it later
-		if not actor:get_data()._hmax and not actor:get_data()._vmax and not actor:get_data()._imspeed and not actor:get_data().imalpha and not actor:get_data().kbimmune and not actor:get_data().stimmune then
+		-- store the enemy's certain stats so we can restore it later
+		if not actor:get_data()._hmax then
 			actor:get_data()._hmax = actor.pHmax
 			actor:get_data()._vmax = actor.pVmax
 			actor:get_data()._imspeed = actor.image_speed
-			actor:get_data().imalpha = actor.image_alpha
-			actor:get_data().kbimmune = actor.knockback_immune
-			actor:get_data().stimmune = actor.stun_immune
+			actor:get_data()._imalpha = actor.image_alpha
+			actor:get_data()._intang = actor.intangible
 		end
 		
 		-- make it not move
 		actor.image_speed = 0.25
 		actor.image_alpha = 0
-		actor.knockback_immune = true
-		actor.stun_immune = true
 		actor.pHmax = 0
 		actor.pVmax = 0
+		actor.intangible = true
 		actor.activity = 50
 		actor.__activity_handler_state = 50
 		actor.state = 0
+		
+		-- (duke nukem voice) shake it baby
+		actor:screen_shake(0.5)
 		
 		-- make it look like its floating
 		if actor.sprite_fall then
@@ -229,10 +290,15 @@ empyorb:onPostStep(function(actor, stack)
 		actor.pHmax = actor:get_data()._hmax
 		actor.pVmax = actor:get_data()._vmax
 		actor.image_speed = actor:get_data()._imspeed
-		actor.image_alpha = actor:get_data().imalpha
-		actor.knockback_immune = actor:get_data().kbimmune
-		actor.stun_immune = actor:get_data().stimmune
+		actor.image_alpha = actor:get_data()._imalpha
+		actor.intangible = actor:get_data()._intang
 		
+		--remove all the temporary variables
+		actor:get_data()._hmax = nil
+		actor:get_data()._vmax = nil
+		actor:get_data()._imspeed = nil
+		actor:get_data()._imalpha = nil
+		actor:get_data()._intang = nil
 		
 		-- give them all elite aspects
 		ssr_give_empyrean_aspects(actor)
@@ -245,17 +311,100 @@ empyorb:onPostStep(function(actor, stack)
 	if gm.inside_view(actor.x, actor.y) == 1 and actor:get_data().empy_beam_over == 1 and actor:get_data().empy_beam <= 0 and Global._current_frame % 2 == 0 then
 		rainbowspark:create_color(actor.x, actor.y, Color.from_hsv((Global._current_frame + actor.y * (0.75)) % 360, 100, 100), 1, Particle.SYSTEM.middle)
 	end
+	
+	if actor.object_index == gm.constants.oWorm then
+		actor.image_blend = Color.from_hsv(Global._current_frame % 360, 65, 100)
+	end
+end)
+
+empyorb:onPostStatRecalc(function(actor)
+	-- giga damage
+	if actor.elite_type then
+		if actor.elite_type == empy.value then
+			actor.damage = actor.damage * (5.4 / 1.9) -- totals to 5.4x
+			actor.cdr = actor.cdr * (0.5 / 0.3) -- totals to 50%
+		end
+	end
+end)
+
+-- empyrean worms !!!
+-- make the worm bodies rainbow too and also make them create sparks
+gm.pre_code_execute("gml_Object_oWormBody_Step_2", function(self, other)
+	if self.parent.elite_type == empy.value then
+		self.image_blend = Color.from_hsv((Global._current_frame + (self.m_id - self.parent.m_id) * 18) % 360, 65, 100)
+		
+		if gm.inside_view(self.x, self.y) == 1 and Global._current_frame % 2 == 0 then
+			rainbowspark:create_color(self.x, self.y, Color.from_hsv((Global._current_frame + self.y * (0.75)) % 360, 100, 100), 1, Particle.SYSTEM.middle)
+		end
+	end
+end)
+
+-- make the worm bodies set their first alarm (needed to shoot the orbs)
+gm.pre_code_execute("gml_Object_oWorm_Alarm_4", function(self, other)
+	if self.elite_type == empy.value then
+		for _, segment in ipairs(self.body) do
+			segment:alarm_set(1, 150 + (segment.m_id - self.m_id) * 2)
+		end
+	end
+end)
+
+-- make the worm bodies shoot the orbs
+gm.pre_code_execute("gml_Object_oWormBody_Alarm_1", function(self, other)
+	if self.parent.elite_type == empy.value then
+		self:alarm_set(1, 330)
+	
+		if self.parent.target then
+			local star = gm.instance_create(self.x, self.y, gm.constants.oWurmMissile)
+			star.parent = self.parent
+			star.target = self.parent.target
+			star.team = self.parent.team
+			star.targetX = self.parent.target.x
+			star.targetY = self.parent.target.y
+			star.lifetime = 30
+			star.damage = self.parent.damage * 0.1
+			if math.random() >= 0.5 then
+				star.direction = self.image_angle - 90 + math.random(-45, 45)
+			else
+				star.direction = self.image_angle + 90 + math.random(-45, 45)
+			end
+			star.speed = star.speed * math.random(1, 2)
+			star.image_alpha = 0.75
+			star.image_blend = self.image_blend
+			
+			self:sound_play(gm.constants.wMedallion, 1, 0.8 + math.random() * 0.2)
+		end
+	end
+end)
+
+-- make the orbs look like colorful stars
+gm.pre_code_execute("gml_Object_oWurmMissile_Draw_0", function(self, other)
+	if self.parent.elite_type == empy.value then
+		gm.draw_sprite_ext(star_sprite, (Global._current_frame % 24) / 3, self.x, self.y, self.image_xscale, self.image_yscale, self.image_angle, self.image_blend, self.image_alpha)
+		gm.draw_set_alpha(0.4)
+		gm.draw_circle_colour(self.x, self.y, 20, self.image_blend, self.image_blend, false)
+		gm.draw_set_alpha(1)
+		if gm.inside_view(self.x, self.y) == 1 and Global._current_frame % 2 == 0 and self.speed > 12 then
+			startrail:create_color(self.x, self.y, self.image_blend, 1, Particle.SYSTEM.middle)
+		end
+		return false -- makes the normal draw code not run, we dont want overlap
+	end
 end)
 
 local blacklist = {
-	["evolvedLemurian"] = true, -- evolved lemurians try not to fuck everything up challenge (impossible) (gone wrong) (called the idpd) (a nuclear missile is heading for your house) (it is 102m away from your house) (sorry not sorry)
-	["lemrider"] = true,
-	["bramble"] = true,
+	["lemrider"] = true, -- the spawn anim breaks since its 2 of them at once
+	["bramble"] = true, -- same issue as above and also requires fixes like the magma worm
+ 	["spitter"] = true, -- technically works but FUCK no
+}
+
+local whitelist = {
+	["jellyfish"] = true,
+	["magmaWorm"] = true,
+	["swift"] = true, -- lmao cope
 }
 
 local all_monster_cards = Monster_Card.find_all()
 for i, card in ipairs(all_monster_cards) do
-	if not blacklist[card.identifier] then
+	if not blacklist[card.identifier] and (card.can_be_blighted == true or whitelist[card.identifier]) then
 		local elite_list = List.wrap(card.elite_list)
 		if not elite_list:contains(empy) then
 			elite_list:add(empy)
