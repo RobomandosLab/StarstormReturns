7ab4fcec71eab828612297d5dc114771fe884ca3
Xaidee <xaidee@duck.com>
2025-11-09 12:00:58 +1300

    Make title sprite replacement configurable with RAPI's ModOptions and gm trickery

--- main.lua
+++ main.lua
@@ -2,13 +2,26 @@
 -- ssr team
 mods["ReturnsAPI-ReturnsAPI"].auto{mp = true, namespace = "ssr"}
 
+--- GLOBALS (Should be in ALL-CAPS for constants, Uppercase Initial for variables)
 PATH = _ENV["!plugins_mod_folder_path"]
+-- mod options stuff
+Options = ModOptions.new("ssr")
+-- Settings with defaults
+-- (useful for setting the default settings when the toml file hasn't been generated
+-- or config options are missing due to a new update adding more)
+Settings = {
+	title_replacement = true,
+}
+SettingsFile = TOML.new()
 
 local init = function()
-	--options = ModOptions.new("ssr")
-    --settings = {}
-    --toml = TOML.new()
-	
+	--- Initialize settings here before requiring anything, useful if some options require a restart to apply.
+	if SettingsFile:read() == nil then
+		SettingsFile:write(Settings)
+	else
+		Settings = SettingsFile:read()
+	end
+
 	local folders = {
 		"Misc", -- contains utility functions that other code depends on, so load first
 		"Language",
@@ -21,7 +34,7 @@ local init = function()
 		--"Artifacts",
 		"Stages"
 	}
-	
+
 	Stage.remove_all_rooms() -- reload stages
 
 	for _, folder in ipairs(folders) do
@@ -34,9 +47,6 @@ local init = function()
 			end
 		end
 	end
-
-	--local saved_settings = toml:read()
-    --if saved_settings then settings = saved_settings end
 
 	-- once we have loaded everything, enable hot/live reloading.
 	-- this variable may be used by content code to make sure it behaves correctly when hotloading
